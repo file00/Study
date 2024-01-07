876676fd8c5a718c3401b66c802ae7d62af93981
file00 <51076009+file00@users.noreply.github.com>
2024-01-07 09:21:04 +0900

    A simple representation of a cat's face

---
+++ Exam_1.c
@@ -0,0 +1,12 @@
+/* 자기 집사를 한심한 표정으로 쳐다보는 고양이 얼굴 표현하기 */
+#include <stdio.h>
+
+int main() {
+	printf("    /\\   /\\\n");
+	printf("   /  \\_/  \\\n");
+	printf("  /  _    _ \\\n");
+	printf(" /   |    |  \\\n");
+	printf(" \\  =  '  =  /\n");
+	printf("  \\   ___   /\n");
+	printf("   \\_______/\n");
+}
