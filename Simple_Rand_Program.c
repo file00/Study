cebda0c08909603991d0bdfb21e6eeea9624f54f
file00 <51076009+file00@users.noreply.github.com>
2024-01-10 13:31:28 +0900

    Add files via upload

---
+++ Simple_Rand_Program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 45
+
+int main(void)
+{ 
+int i;
+srand( (unsigned)time( NULL ) );
+for( i = 0; i < 6; i++ )
+printf("%d ", 1+(rand()%MAX) );
+return 0; 
+}
