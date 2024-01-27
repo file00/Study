199ddcac900b35b72e09f3c196c7931ea3aa0c69
file00 <51076009+file00@users.noreply.github.com>
2024-01-27 19:29:14 +0900

    Add files via upload

---
+++ typedef.c
@@ -0,0 +1,13 @@
+/* typedef */
+#include <stdio.h>
+
+typedef unsigned short int U;
+
+int main() {
+    unsigned short int data = 4;
+    U temp;
+    temp = data;
+    printf("%d", temp);
+
+    return 0;
+}
