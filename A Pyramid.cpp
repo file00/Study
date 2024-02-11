aade4d38d8d736f0751e31f066a19b20bb86704a
file00 <51076009+file00@users.noreply.github.com>
2024-02-11 11:49:38 +0900

    Update A Pyramid.cpp

--- A Pyramid.cpp
+++ A Pyramid.cpp
@@ -1,22 +1,21 @@
 /* A Pyramid */
 #include <iostream>
 using namespace std;
-int main()
-{
-    int Size = 11, i, j, middle = Size / 2;
-    
-    for(i = 0; i <= middle + 1; i++) {
-        for(j = 0; j < Size; j++) {
+int main() {
+    int Size;
+    cin >> Size;
+    int i, j, middle = Size / 2;
+
+    for (i = 0; i <= middle + 1; i++) {
+        for (j = 0; j < Size; j++) {
             if (j > middle - i && j < middle + i) {
                 cout << "*";
             }
             else {
-                cout<<" ";
+                cout << " ";
             }
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
-
-
