4b77bcc0606ea5d7f92f997336db5c9f5b7c3e86
file00 <51076009+file00@users.noreply.github.com>
2024-02-12 11:36:16 +0900

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,15 @@
+/* Pointer */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 5;
+	int* p = &x;
+	cout << "x = " << x << endl;
+
+	*p = 6;
+	cout << "x = " << x << endl;
+	return 0;
+}
