dd2124e677f842abf7d021d413bbd8dc027e5a08
file00 <51076009+file00@users.noreply.github.com>
2024-02-16 01:20:47 +0900

    Add files via upload

---
+++ Half Pyramid.c
@@ -0,0 +1,17 @@
+/* Half Pyramid */
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+    int i, j, rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
+    for (i = 1; i < rows; i++) {
+        for (j = 1; j < i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
