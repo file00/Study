4d7e045d96bcb53a54017bbd835faa1412720f94
file00 <51076009+file00@users.noreply.github.com>
2024-02-17 01:24:40 +0900

    Update An ASCII Code.cpp

--- An ASCII Code.cpp
+++ An ASCII Code.cpp
@@ -10,3 +10,28 @@ int main() {
 	}
 	return 0;
 }
+
+/* Get ASCII Value of a Character */
+
+/*int main() {
+    char ch = 'a';
+
+    cout << "ASCII value = " << int(ch) << endl;
+
+    return 0;
+}*/
+
+// Printing the corresponding ASCII of a character 
+// Notice the use of int() to get an integer
+
+/* Print Character Using ASCII Value */
+
+/*int main() {
+    // assigning an integer value to char
+    char ch = 104;
+
+    // printing the variable
+    cout << "Character = " << ch << endl;
+
+    return 0;
+}*/
