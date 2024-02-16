5bab7aa00f70b9fd2c0cbea373c15b3f3068ecb1
file00 <51076009+file00@users.noreply.github.com>
2024-02-16 20:27:10 +0900

    Update Half Pyramid.c

--- Half Pyramid.c
+++ Half Pyramid.c
@@ -17,3 +17,19 @@ int main() {
     }
     return 0;
 }
+
+/* Half Pyramid of Alphabets */ // As the title reads, alphabetic capital letters are printed.
+int main() {
+   int i, j;
+   char input, alphabet = 'A';
+   printf("Enter an uppercase character you want to print in the last row: ");
+   scanf("%c", &input);
+   for (i = 1; i <= (input - 'A' + 1); ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%c ", alphabet);
+      }
+      ++alphabet;
+      printf("\n");
+   }
+   return 0;
+}
