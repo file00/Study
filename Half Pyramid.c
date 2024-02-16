e4bd6b9c62aec91be73327bfcf796b066505bcaa
file00 <51076009+file00@users.noreply.github.com>
2024-02-16 20:22:58 +0900

    Update Half Pyramid.c

--- Half Pyramid.c
+++ Half Pyramid.c
@@ -7,10 +7,10 @@ int main() {
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
     
-    for (i = 1; i < rows; i++) {
-        for (j = 1; j < i; j++) {
-            // printf("* "); // Use if you want to print out in the shape of '*'
-            // printf("%d", rows); //In this case, only the numbers entered have a shape and are output.
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            // printf("* "); // Use if you want to print out in the shape of '*'.
+            // printf("%d", rows); // In this case, only the numbers entered have a shape and are output.
             printf("%d", j); 
         }
         printf("\n");
