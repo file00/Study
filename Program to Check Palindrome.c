0339d4d81a35c5f51b0f6407544f0f9cc5e4c0da
file00 <51076009+file00@users.noreply.github.com>
2024-02-27 00:10:06 +0900

    Add files via upload

---
+++ Program to Check Palindrome.c
@@ -0,0 +1,25 @@
+/* Program to Check Palindrome */
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+
+    // reversed integer is stored in reversed variable
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignal and reversed are equal
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
