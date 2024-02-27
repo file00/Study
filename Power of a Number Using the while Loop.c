ea2b32c421044994d55119a08ed8e0998ad77077
file00 <51076009+file00@users.noreply.github.com>
2024-02-28 00:07:29 +0900

    Add files via upload

---
+++ Power of a Number Using the while Loop.c
@@ -0,0 +1,19 @@
+/* Power of a Number Using the while Loop */
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int base, exp;
+    long double result = 1.0;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+
+    while (exp != 0) {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %.0Lf", result);
+    return 0;
+}
