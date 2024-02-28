8ffe91a96883b347eee93295fa385fe35fc69cec
file00 <51076009+file00@users.noreply.github.com>
2024-02-29 00:13:35 +0900

    Add files via upload

---
+++ Power Using pow() Function.c
@@ -0,0 +1,18 @@
+/* Power Using pow() Function */
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    double base, exp, result;
+    printf("Enter a base number: ");
+    scanf("%lf", &base);
+    printf("Enter an exponent: ");
+    scanf("%lf", &exp);
+
+    // calculates the power
+    result = pow(base, exp);
+
+    printf("%.1lf^%.1lf = %.2lf", base, exp, result);
+    return 0;
+}
