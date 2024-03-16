06fe2618a59c3d8566538345aabcd2db9a0ec4a0
file00 <51076009+file00@users.noreply.github.com>
2024-03-16 16:44:11 +0900

    Add files via upload

---
+++ C Structure and Function.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* Passing structs to functions */
+struct student {
+   char name[50];
+   int age;
+};
+
+void display(struct student s);
+
+int main() {
+  struct student s1;
+
+  printf("Enter name: ");
+
+  // read string input from the user until \n is entered
+  // \n is discarded
+  scanf("%[^\n]%*c", s1.name);
+
+  printf("Enter age: ");
+  scanf("%d", &s1.age);
+
+  display(s1); // passing struct as an argument
+
+  return 0;
+}
+
+void display(struct student s) {
+  printf("\nDisplaying information\n");
+  printf("Name: %s", s.name);
+  printf("\nAge: %d", s.age);
+}
+
+/* Return struct from a function */
+// For structure, use the ones in line5 to line8 as they are.
+
+// struct student getInformation();
+
+// int main() {
+//     struct student s;
+    
+//     s = getInformation();
+    
+//     printf("\nDisplaying information\n");
+//     printf("Name: %s", s.name);
+//     printf("\nRoll: %d", s.age);
+    
+//     return 0;
+// }
+
+// struct student getInformation() {
+//     struct student s1;
+    
+//     printf("Enter name: ");
+//     scanf("%[^\n]%*c", s1.name);
+    
+//     printf("Enter age: ");
+//     scanf("%d", &s1.age);
+
+//     return s1;
+// }
