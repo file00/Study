05252fda5ad4948f8ce81dc3f92b2ce00eea29bf
file00 <51076009+file00@users.noreply.github.com>
2024-03-19 07:31:27 +0900

    Update and rename A first program.c to Program to Display "Hello, World!".c

--- Program to Display "Hello, World!".c
+++ Program to Display "Hello, World!".c
@@ -4,6 +4,10 @@ void main() {
 	printf("Hello~ world!!\n");  
 }
 
+//int main() {
+//      printf("Hello~ world!!\n");  
+//}
+
 // Code Interpretation
 // 1. #include <stdio.h> == Directs the file named stdio.h to be read for file input/output.
 // 2. main() == You can understand it as the starting point of the C language program.
