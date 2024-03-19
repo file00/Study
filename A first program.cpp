cf5929172249044ccf20f03f36779fd542bedff1
file00 <51076009+file00@users.noreply.github.com>
2024-03-19 09:06:30 +0900

    Update A first program.cpp

--- A first program.cpp
+++ A first program.cpp
@@ -7,3 +7,14 @@ int main() {
 
     return 0;
 }
+
+/* Numbers and Characters Output */
+// int main() {
+//     int num1 = 70;
+//     double num2 = 256.783;
+//     char ch = 'A';
+//     cout << num1 << endl;
+//     cout << num2 << endl;
+//     cout << "Characters: " << ch << endl;
+//     return 0;
+// }
