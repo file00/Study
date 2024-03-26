293310943a303a77a2823d5bf456f0af99873ab2
file00 <51076009+file00@users.noreply.github.com>
2024-03-27 00:24:00 +0900

    Update Sum of Positive Numbers Only.cpp

--- Sum of Positive Numbers Only.cpp
+++ Sum of Positive Numbers Only.cpp
@@ -29,3 +29,24 @@ int main() {
     
     return 0;
 }
+
+
+/* Sum of Positive Numbers Only (do while Version) */
+// int main() {
+//     int number = 0;
+//     int sum = 0;
+
+//     do {
+//         sum += number;
+
+//         // take input from the user
+//         cout << "Enter a number: ";
+//         cin >> number;
+//     }
+//     while (number >= 0);
+    
+//     // display the sum
+//     cout << "\nThe sum is " << sum << endl;
+    
+//     return 0;
+// }
