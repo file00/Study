bd4b49309cd4a87c8deacaaf44573ff5cbcd0793
file00 <51076009+file00@users.noreply.github.com>
2024-03-30 19:16:04 +0900

    Add files via upload

---
+++ Random numbering.c
@@ -0,0 +1,19 @@
+/* Random numbering */
+
+// Since the numbers are randomly outputted from 1 to 9, different numbers come out every time the code is outputted.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int i, random;
+    
+    srand(time(NULL));
+    for (i = 0; i < 10; i++) {
+        random = rand() % 5;
+        printf("%d ", random);
+    }
+    return 0;
+}
+
+
