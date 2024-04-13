b7f0d01df6f7acfad9e9b4fb752d4d0cb864f600
file00 <51076009+file00@users.noreply.github.com>
2024-04-14 04:32:49 +0900

    Add files via upload

---
+++ Program to Print an Integer.cpp
@@ -0,0 +1,17 @@
+/* Program to Print an Integer */
+using namespace std;
+#include <iostream>
+
+int main() {
+    int number;
+    
+    cout << "Enter an integer: ";  
+    
+    // reads and stores input
+    cin >> number;
+
+    // displays output
+    cout << "You entered: " << number << endl;
+
+    return 0;
+}
