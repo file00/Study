0f32378780f4613249c8650a15b23a0ad3fd9001
file00 <51076009+file00@users.noreply.github.com>
2024-04-14 04:44:53 +0900

    Add files via upload

---
+++ Predefined Macros.cpp
@@ -0,0 +1,14 @@
+/* Predefined Macros */
+using namespace std;
+#include <iostream>
+
+int main() {
+
+    cout << "Current date: " << __DATE__ << endl;
+    cout << "Current file: " << __FILE__ << endl;
+    cout << "Current line: " << __LINE__ << endl;
+    cout << "Current stdc: " << __STDC__ << endl;
+    cout << "Current time: " << __TIME__ << endl;
+   
+    return 0;
+}
