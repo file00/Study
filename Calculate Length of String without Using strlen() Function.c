00f328e5a4ead776c1bd211abbaae6355b9be580
file00 <51076009+file00@users.noreply.github.com>
2024-04-19 12:37:11 +0900

    Update Calculate Length of String without Using strlen() Function.c

--- Calculate Length of String without Using strlen() Function.c
+++ Calculate Length of String without Using strlen() Function.c
@@ -3,11 +3,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    char s[] = "Programming is fun";
+    char s[40];
     int i;
 
+    printf("Input: ");
+    fgets(s, 40, stdin);
+
     for (i = 0; s[i] != '\0'; ++i);
-    
     printf("Length of the string: %d", i);
+    
     return 0;
 }
