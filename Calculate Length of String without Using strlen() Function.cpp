84c44ce54051dc963407f0e8e8e8d0eb579387ed
file00 <51076009+file00@users.noreply.github.com>
2024-04-19 12:47:42 +0900

    Update Calculate Length of String without Using strlen() Function.cpp

--- Calculate Length of String without Using strlen() Function.cpp
+++ Calculate Length of String without Using strlen() Function.cpp
@@ -1,13 +1,17 @@
 /* Calculate Length of String without Using strlen() Function */
+#include <cstdio>
 #include <iostream>
 using namespace std;
 
 int main() {
-    char s[] = "Programming is fun";
+    char s[40];
     int i;
 
+    cout << "Input: ";
+    fgets(s, 40, stdin);
+
     for (i = 0; s[i] != '\0'; ++i);
-    
     cout << "Length of the string: " << i << endl;
+    
     return 0;
 }
