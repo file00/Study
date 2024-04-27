217972c46dade6e8c4559f29a7a870b93b88c012
file00 <51076009+file00@users.noreply.github.com>
2024-04-28 07:47:31 +0900

    Update Factorial of a Number Using Recursion.cpp

--- Factorial of a Number Using Recursion.cpp
+++ Factorial of a Number Using Recursion.cpp
@@ -24,3 +24,13 @@ int factorial(int n) {
         return 1;
     }
 }
+
+// Advantages and Disadvantages of Recursion
+// Advantages of C++ Recursion
+// It makes our code shorter and cleaner.
+// Recursion is required in problems concerning data structures and advanced algorithms, such as Graph and Tree Traversal.
+
+// Disadvantages of C++ Recursion
+// It takes a lot of stack space compared to an iterative program.
+// It uses more processor time.
+// It can be more difficult to debug compared to an equivalent iterative program.
