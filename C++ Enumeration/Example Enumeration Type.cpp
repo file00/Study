8dab99cc97a2009f66472d28a5fc4fe805ddbddc
file00 <51076009+file00@users.noreply.github.com>
2024-06-10 08:58:43 +0900

    Add files via upload

---
+++ C++ Enumeration/Example Enumeration Type.cpp
@@ -0,0 +1,13 @@
+/* Example: Enumeration Type */
+
+#include <iostream>
+using namespace std;
+
+enum week { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
+int main() {
+    week today;
+    today = Wednesday;
+    cout << "Day " << today+1;
+    return 0;
+}
