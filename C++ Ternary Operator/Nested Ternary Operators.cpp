ea21b4a2b3368f81c060b7730d45fe28a5e305bb
file00 <51076009+file00@users.noreply.github.com>
2024-06-18 20:30:25 +0900

    Add files via upload

---
+++ C++ Ternary Operator/Nested Ternary Operators.cpp
@@ -0,0 +1,17 @@
+/* Nested Ternary Operators */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int number = 0;
+  string result;
+
+  // nested ternary operator to find whether
+  // number is positive, negative, or zero
+  result = (number == 0) ? "Zero" : ((number > 0) ? "Positive" : "Negative");
+
+  cout << "Number is " << result;
+
+  return 0;
+}
