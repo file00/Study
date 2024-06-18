47d0d97eaf0e679e5953357540aefbb850e1d0b9
file00 <51076009+file00@users.noreply.github.com>
2024-06-18 18:49:01 +0900

    Add files via upload

---
+++ C++ Functions/Display a Text.cpp
@@ -0,0 +1,15 @@
+/* Display a Text */
+#include <iostream>
+using namespace std;
+
+// declaring a function
+void greet() {
+    cout << "Hello there!";
+}
+
+int main() {
+    // calling the function
+    greet();
+
+    return 0;
+}
