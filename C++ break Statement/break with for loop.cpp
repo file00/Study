7d5b98b49101fbdd53be828ee073058c69f0434d
file00 <51076009+file00@users.noreply.github.com>
2024-06-20 10:45:03 +0900

    Add files via upload

---
+++ C++ break Statement/break with for loop.cpp
@@ -0,0 +1,17 @@
+/* break with for loop */
+// program to print the value of i
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        // break condition     
+        if (i == 3) {
+            break;
+        }
+        cout << i << endl;
+    }
+
+return 0;
+}
