d749d96a42185ee8024cfeaed8859dd6f0f3cdfd
file00 <51076009+file00@users.noreply.github.com>
2024-06-20 10:38:01 +0900

    Add files via upload

---
+++ C++ continue Statement/continue with Nested loop.cpp
@@ -0,0 +1,25 @@
+/* continue with Nested loop */
+// using continue statement inside
+// nested for loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int sum = 0;
+
+    // nested for loops
+    // first loop
+    for (int i = 1; i <= 3; i++) {
+        // second loop
+        for (int j = 1; j <= 3; j++) {
+            if (j == 2) {
+                continue;
+            }
+            cout << "i = " << i << ", j = " << j << endl;
+        }
+    }
+
+    return 0;
+}
