5e0ef8d515b5a30afe784ad670c290ccff4ff489
file00 <51076009+file00@users.noreply.github.com>
2024-06-20 14:19:59 +0900

    Update Display Numbers from 1 to 5.cpp

--- C++ while and do while Loop/Display Numbers from 1 to 5.cpp
+++ C++ while and do while Loop/Display Numbers from 1 to 5.cpp
@@ -14,3 +14,21 @@ int main() {
     }
     return 0;
 }
+
+// do_version
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int i = 1; 
+
+//     // do...while loop from 1 to 5
+//     do {
+//         cout << i << " ";
+//         ++i;
+//     }
+//     while (i <= 5);
+    
+//     return 0;
+// }
