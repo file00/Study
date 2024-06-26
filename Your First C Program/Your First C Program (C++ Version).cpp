f1b8b46f3be8eafdc2ade54181cb1d0ef1409bbf
file00 <51076009+file00@users.noreply.github.com>
2024-06-26 10:40:09 +0900

    Add files via upload

---
+++ Your First C Program/Your First C Program (C++ Version).cpp
@@ -0,0 +1,9 @@
+/* Your First C Program (C++ Version) */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; // print Hello World to the screen
+
+    return 0;
+}
