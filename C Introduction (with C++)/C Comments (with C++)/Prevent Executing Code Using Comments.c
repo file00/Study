39fed9d9179ad3750280f0bc951193f2b8ca6af2
file00 <51076009+file00@users.noreply.github.com>
2024-07-09 07:42:24 +0900

    Add files via upload

---
+++ C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments.c
@@ -0,0 +1,12 @@
+/* Prevent Executing Code Using Comments */
+#include <stdio.h>
+
+int main() {
+    int number1 = 10;
+    int number2 = 15;
+    int sum = number1 + number2;
+
+    printf("The sum is: %d\n", sum);
+    // printf("The product is: %d\n", product); 
+    return 0;
+}
