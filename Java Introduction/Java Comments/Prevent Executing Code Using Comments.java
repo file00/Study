49970eed10d2368adca73dd1ac53e6034bcca976
file00 <51076009+file00@users.noreply.github.com>
2024-07-10 00:06:18 +0900

    Delete Java Introduction directory

--- Java Introduction/Java Comments/Prevent Executing Code Using Comments.java
+++
@@ -1,9 +0,0 @@
-/* Prevent Executing Code Using Comments */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("some code");
-        // System.out.println("error code");
-        System.out.println("some other code");
-
-    }
-}
--- Java Introduction/My First Java Program/My First Java Program.java
+++
@@ -1,8 +0,0 @@
-/* My First Java Program */
-
-class Main {
-    public static void main(String[] args) {
-        // print Hello World to the screen
-        System.out.println("Hello World");
-    }
-}
