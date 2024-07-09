6265b01d7770b9232f0fd757a82c57d1cd4d10eb
file00 <51076009+file00@users.noreply.github.com>
2024-07-09 09:03:18 +0900

    Delete Python Introduction directory

--- Python Introduction/Python Comments/Prevent Executing Code Using Comments.py
+++
@@ -1,8 +0,0 @@
-# Prevent Executing Code Using Comments
-number1 = 10
-number2 = 15
-
-sum = number1 + number2
-
-print("The sum is:", sum)
-# print('The product is:', product)
--- Python Introduction/Python Comments/Single-line Comment.py
+++
@@ -1,6 +0,0 @@
-# Single-line Comment
-# declare a variable
-name = "John"
-
-# print name
-print(name)    # John
