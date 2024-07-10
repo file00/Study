dd14ce455d7c097b78fe2452529d409c22779618
file00 <51076009+file00@users.noreply.github.com>
2024-07-10 11:06:26 +0900

    Update Integer Literals.java

--- Java/Java Fundamentals/Java Variables and Literals/Integer Literals.java
+++ Java/Java Fundamentals/Java Variables and Literals/Integer Literals.java
@@ -1,23 +1,31 @@
-/* Integer Literals */
 class Main {
-  public static void main(String[] args) {
-    // binary
-    int binaryNumber = 0b10010;
-    // octal 
-    int octalNumber = 027;
+    public static void main(String[] args) {
 
-    // decimal
-    int decNumber = 34;
+        // binary
+        int binaryNumber = 0b10010;
+        // octal
+        int octalNumber = 027;
+        // decimal
+        int decNumber = 34;
+        // hexadecimal
+        int hexNumber = 0x2F; // 0x represents hexadecimal
+        // binary
+        int binNumber = 0b10010; // 0b represents binary
 
-    // hexadecimal 
-    int hexNumber = 0x2F; // 0x represents hexadecimal
-    // binary
-    int binNumber = 0b10010; // 0b represents binary
+        double myDouble = 3.4;
+        float myFloat = 3.4F;
 
-    System.out.println(binNumber);  
-    System.out.println(octalNumber);    
-    System.out.println(decNumber);  
-    System.out.println(hexNumber);  
-    System.out.println(binNumber);    
-  }
+        // 3.445*10^2
+        double myDoubleScientific = 3.445e2;
+
+        System.out.println(binaryNumber);
+        System.out.println(octalNumber);
+        System.out.println(decNumber);
+        System.out.println(hexNumber);
+        System.out.println(binNumber);
+
+        System.out.println(myDouble);  // prints 3.4
+        System.out.println(myFloat);    // prints 3.4
+        System.out.println(myDoubleScientific);   // prints 344.5
+    }
 }
