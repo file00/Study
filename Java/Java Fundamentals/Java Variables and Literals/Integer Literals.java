e7f5edbbfdc5ce450bfe23978ecc4935d7e695ff
file00 <51076009+file00@users.noreply.github.com>
2024-07-10 11:08:07 +0900

    Update Integer Literals.java

--- Java/Java Fundamentals/Java Variables and Literals/Integer Literals.java
+++ Java/Java Fundamentals/Java Variables and Literals/Integer Literals.java
@@ -13,20 +13,10 @@ class Main {
         // binary
         int binNumber = 0b10010; // 0b represents binary
 
-        double myDouble = 3.4;
-        float myFloat = 3.4F;
-
-        // 3.445*10^2
-        double myDoubleScientific = 3.445e2;
-
         System.out.println(binaryNumber);
         System.out.println(octalNumber);
         System.out.println(decNumber);
         System.out.println(hexNumber);
         System.out.println(binNumber);
-
-        System.out.println(myDouble);  // prints 3.4
-        System.out.println(myFloat);    // prints 3.4
-        System.out.println(myDoubleScientific);   // prints 344.5
     }
 }
