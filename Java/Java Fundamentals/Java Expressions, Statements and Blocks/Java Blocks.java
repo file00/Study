ac4f9a2f9585849b6626f013e06f29a5451e81e8
file00 <51076009+file00@users.noreply.github.com>
2024-07-12 00:07:14 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Expressions, Statements and Blocks/Java Blocks.java
@@ -0,0 +1,12 @@
+/* Java Blocks */
+class Main {
+    public static void main(String[] args) {
+    	
+        String band = "Beatles";
+    	
+        if (band == "Beatles") { // start of block
+            System.out.print("Hey ");
+            System.out.print("Jude!");
+        } // end of block
+    }
+}
