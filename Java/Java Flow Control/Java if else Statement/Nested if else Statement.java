8a3de7a351bb0f2f458887e3beca183540629e5e
file00 <51076009+file00@users.noreply.github.com>
2024-07-14 11:14:12 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java if else Statement/Nested if else Statement.java
@@ -0,0 +1,35 @@
+/* Nested if...else Statement */
+class Main {
+  public static void main(String[] args) {
+
+    // declaring double type variables
+    Double n1 = -1.0, n2 = 4.5, n3 = -5.3, largest;
+
+    // checks if n1 is greater than or equal to n2
+    if (n1 >= n2) {
+
+      // if...else statement inside the if block
+      // checks if n1 is greater than or equal to n3
+      if (n1 >= n3) {
+        largest = n1;
+      }
+
+      else {
+        largest = n3;
+      }
+    } else {
+
+      // if..else statement inside else block
+      // checks if n2 is greater than or equal to n3
+      if (n2 >= n3) {
+        largest = n2;
+      }
+
+      else {
+        largest = n3;
+      }
+    }
+
+    System.out.println("Largest Number: " + largest);
+  }
+}
