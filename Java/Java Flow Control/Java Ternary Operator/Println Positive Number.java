46073b133bec071866608193460ce1cc00ddf1b1
file00 <51076009+file00@users.noreply.github.com>
2024-07-18 16:51:25 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java Ternary Operator/Println Positive Number.java
@@ -0,0 +1,15 @@
+/* Println Positive Number */
+class Main {
+  public static void main(String[] args) {
+    
+    // create a variable 
+    int number = 24;
+
+    if(number > 0) {
+      System.out.println("Positive Number");
+    }
+    else {
+      System.out.println("Negative Number");
+    }
+  }
+}
