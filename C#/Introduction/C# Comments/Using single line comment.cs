b83f8cfbadfe446e435f2dcee9c91b121d04a835
file00 <51076009+file00@users.noreply.github.com>
2024-07-19 12:46:47 +0900

    Update Using single line comment.cs

--- C#/Introduction/C# Comments/Using single line comment.cs
+++ C#/Introduction/C# Comments/Using single line comment.cs
@@ -2,9 +2,12 @@
 // Hello World Program
 using System;
 
-namespace HelloWorld {
-	class Program {
-		public static void Main(string[] args) { // Execution Starts from Main method
+namespace HelloWorld 
+{
+	class Program 
+	{
+		public static void Main(string[] args) 
+		{ // Execution Starts from Main method
 			// Prints Hello World
 			Console.WriteLine("Hello World!");
 		}
