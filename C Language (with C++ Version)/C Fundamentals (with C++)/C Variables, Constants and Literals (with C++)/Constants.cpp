7164d0a5ac98f23b69fea4ec67d1d0cd6d3a11a4
file00 <51076009+file00@users.noreply.github.com>
2024-07-21 00:36:05 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Constants.cpp
@@ -0,0 +1,11 @@
+/* Constants */
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14;
+    
+    cout << PI << endl;
+    
+    return 0;
+}
---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Variables.cpp
@@ -0,0 +1,16 @@
+/* Variables */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    char ch = 'a';
+    
+    cout << age << endl;
+    cout << ch << endl;
+    
+    ch = 'l';
+    cout << ch << endl;
+    
+    return 0;
+}
