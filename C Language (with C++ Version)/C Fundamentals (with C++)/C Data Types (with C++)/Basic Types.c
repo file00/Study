2a2e747f941f23bfdb8fabc5b12c5c138849846e
file00 <51076009+file00@users.noreply.github.com>
2024-07-22 06:40:07 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Data Types (with C++)/Basic Types.c
@@ -0,0 +1,23 @@
+/* Basic Types */
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>      
+int main() {
+    int i;
+    float f;
+    double db;
+    char ch = 'A';
+    short a;
+    long b;
+    long long c;
+    long double d;
+    
+    printf("size of int = %ld bytes\n", sizeof(i));
+    printf("size of float = %ld bytes\n", sizeof(f));
+    printf("size of double = %ld bytes\n", sizeof(db));
+    printf("size of char = %ld bytes\n", sizeof(ch));
+    printf("size of short = %ld bytes\n", sizeof(a));
+    printf("size of long = %ld bytes\n", sizeof(b));
+    printf("size of long long = %ld bytes\n", sizeof(c));
+    printf("size of long double= %ld bytes\n", sizeof(d));
+  return 0;
+}
