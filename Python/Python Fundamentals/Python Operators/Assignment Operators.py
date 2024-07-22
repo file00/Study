13cc26fa38c78a41bf7d26c142374e8473d1ff5c
file00 <51076009+file00@users.noreply.github.com>
2024-07-22 09:21:29 +0900

    Add files via upload

---
+++ Python/Python Fundamentals/Python Operators/Assignment Operators.py
@@ -0,0 +1,11 @@
+# Assignment Operators
+# assign 10 to a
+a = 10
+
+# assign 5 to b
+b = 5 
+
+# assign the sum of a and b to a
+a += b      # a = a + b
+
+print(a)
---
+++ Python/Python Fundamentals/Python Operators/Comparison Operators.py
@@ -0,0 +1,22 @@
+# Comparison Operators
+a = 5
+
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
