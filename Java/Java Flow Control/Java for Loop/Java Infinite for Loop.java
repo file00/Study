9f18fa191f46157d6b551afc68110c23ad5e535a
file00 <51076009+file00@users.noreply.github.com>
2024-07-24 00:28:02 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java for Loop/Java Infinite for Loop.java
@@ -0,0 +1,14 @@
+/** Java Infinite for Loop **/
+
+// Infinite for Loop
+
+class Main {
+    public static void main(String[] args) {
+      
+        int sum = 0;
+
+        for (int i = 1; i <= 10; --i) {
+            System.out.println("Hello");
+        }
+    }
+}
---
+++ Java/Java Flow Control/Java for Loop/Java for-each Loop.java
@@ -0,0 +1,16 @@
+/** Java for-each Loop **/
+
+// print array elements 
+
+class Main {
+  public static void main(String[] args) {
+      
+    // create an array
+    int[] numbers = {3, 7, 5, -5};
+    
+    // iterating through the array 
+    for (int number: numbers) {
+       System.out.println(number);
+    }
+  }
+}
