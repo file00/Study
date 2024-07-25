96011c6573a69167d47c45ac51db0e803085b4d0
file00 <51076009+file00@users.noreply.github.com>
2024-07-25 22:39:15 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Float and Double Input & Output.cpp
@@ -0,0 +1,17 @@
+/* Float and Double Input/Output */
+#include <iostream>
+using namespace std;
+int main() {
+    float num1;
+    double num2;
+    
+    cout << "Enter a number: ";
+    cin >> num1;
+    cout << "Enter another number: ";
+    cin >> num2;
+    
+    cout << "num1 = " << num1 << endl;
+    cout << "num2 = " << num2 << endl;
+
+    return 0;
+}
