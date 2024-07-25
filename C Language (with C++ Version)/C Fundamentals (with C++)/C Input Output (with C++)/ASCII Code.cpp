5678e7b90922ac9a4b2a6efaa15bf8401a499fec
file00 <51076009+file00@users.noreply.github.com>
2024-07-25 15:33:00 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Code.cpp
@@ -0,0 +1,37 @@
+/* ASCII Code */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i;
+	for (i = 97; i < 100; i++) {
+		cout << i << " " << char(i) << endl;
+	}
+	return 0;
+}
+
+/* Get ASCII Value of a Character */
+
+/*int main() {
+    char ch = 'a';
+
+    cout << "ASCII value = " << int(ch) << endl;
+
+    return 0;
+}*/
+
+// Printing the corresponding ASCII of a character 
+// Notice the use of int() to get an integer
+
+/* Print Character Using ASCII Value */
+
+/*int main() {
+    // assigning an integer value to char
+    char ch = 104;
+
+    // printing the variable
+    cout << "Character = " << ch << endl;
+
+    return 0;
+}*/
