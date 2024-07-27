2c24659342381093a3eb1129a92d00395ceea4e8
file00 <51076009+file00@users.noreply.github.com>
2024-07-27 11:10:32 +0900

    Add files via upload

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/C++ Character I & O.cpp
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/C++ Character I & O.cpp
@@ -1,12 +1,13 @@
 /* C++ Character I/O */
 #include <iostream>
 using namespace std;
+
 int main() {
     char chr;
     
-    cout << "Enter a character: ";
-    cin >> chr;
-    cout << "You entered "<< chr << "." << endl;  
+    cout << "Enter a character: " << chr;
+    cin >> &chr;     
+    cout << "You entered " << chr << "." << endl;  
 
     return 0;
 }
