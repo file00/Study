f9d474a2d81d5255f1b981469d569bbda393a619
file00 <51076009+file00@users.noreply.github.com>
2024-07-27 10:14:59 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Output.cpp
@@ -0,0 +1,11 @@
+/* Integer Output */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testInteger = 5;
+    
+    cout << "Number = " << testInteger << endl;
+
+    return 0;
+}
---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Print Characters.cpp
@@ -0,0 +1,11 @@
+/* Print Characters */
+#include <iostream>
+using namespace std;
+
+int main() {
+    char chr = 'a';    
+     
+    cout << "character = " << chr << endl;
+
+    return 0;
+}
