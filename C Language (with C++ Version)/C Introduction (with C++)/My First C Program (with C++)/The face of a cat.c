b6062e1f5f808f7659e63b2c887c882c51154e58
file00 <51076009+file00@users.noreply.github.com>
2024-07-27 10:17:38 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/The face of a cat.c
@@ -0,0 +1,12 @@
+/* The face of a cat */
+#include <stdio.h>
+
+int main() {
+	printf("    /\\   /\\\n");
+	printf("   /  \\_/  \\\n");
+	printf("  /  _    _ \\\n");
+	printf(" /   |    |  \\\n");
+	printf(" \\  =  '  =  /\n");
+	printf("  \\   ___   /\n");
+	printf("   \\_______/\n");
+}
