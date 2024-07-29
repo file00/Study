1a1624676c32fffc3c52d53bda6da423fd343959
file00 <51076009+file00@users.noreply.github.com>
2024-07-29 18:13:03 +0900

    Add files via upload

---
+++ C#/Introduction/C# Basic Input and Output/Printing String using WriteLine().cs
@@ -0,0 +1,14 @@
+/* Printing String using WriteLine() */
+
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("C# is cool");
+		}
+	}
+}
