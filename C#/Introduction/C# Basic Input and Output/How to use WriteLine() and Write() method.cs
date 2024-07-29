db338552086ad8f5c0db0ac1dd559ab04f2512e0
file00 <51076009+file00@users.noreply.github.com>
2024-07-29 18:15:48 +0900

    Add files via upload

---
+++ C#/Introduction/C# Basic Input and Output/How to use WriteLine() and Write() method.cs
@@ -0,0 +1,18 @@
+/* How to use WriteLine() and Write() method? */
+
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Prints on ");
+			Console.WriteLine("New line");
+
+			Console.Write("Prints on ");
+			Console.Write("Same line");
+		}
+	}
+}
