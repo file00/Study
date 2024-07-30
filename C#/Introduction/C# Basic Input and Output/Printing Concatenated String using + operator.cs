65b69b26b7420cec51fe37d92a49681f11634d4d
file00 <51076009+file00@users.noreply.github.com>
2024-07-30 12:20:32 +0900

    Add files via upload

---
+++ C#/Introduction/C# Basic Input and Output/Printing Concatenated String using + operator.cs
@@ -0,0 +1,15 @@
+/* Printing Concatenated String using + operator */
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int val = 55;
+			Console.WriteLine("Hello " + "World");
+			Console.WriteLine("Value = " + val);
+		}
+	}
+}
---
+++ C#/Introduction/C# Basic Input and Output/Printing Variables and Literals.cs
@@ -0,0 +1,18 @@
+/* Printing Variables and Literals */
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int value = 10;
+
+			// Variable
+			Console.WriteLine(value);
+			// Literal
+			Console.WriteLine(50.05);
+		}
+	}
+}
