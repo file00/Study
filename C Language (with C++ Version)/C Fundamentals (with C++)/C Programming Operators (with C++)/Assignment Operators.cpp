8e07ca9d2e93d2bb6469982b21c497eca68eed97
file00 <51076009+file00@users.noreply.github.com>
2024-08-03 11:18:00 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Assignment Operators.cpp
@@ -0,0 +1,28 @@
+/* Assignment Operators */
+// Working of assignment operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, c;
+    
+    c = a;
+    cout << "c = " << c << endl;
+    c += a;
+    cout << "c = " << c << endl;
+    c -= a;
+    cout << "c = " << c << endl;
+    c *= a;
+    cout << "c = " << c << endl;
+    c /= a;
+    cout << "c = " << c << endl;
+    c %= a;
+    cout << "c = " << c << endl;
+
+    return 0;
+}
+
+
+
+   
+
