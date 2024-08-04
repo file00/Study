79a64a8076bd9e315ce3cdf4d4d9ab0d4cf0d6e4
file00 <51076009+file00@users.noreply.github.com>
2024-08-05 07:27:02 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Flow Control (with C++)/C for Loop/for loop.c
@@ -0,0 +1,13 @@
+/* for loop */
+// Print numbers from 1 to 10
+#include <stdio.h>
+
+int main() {
+  int i;
+
+  for (i = 1; i < 11; ++i)
+  {
+    printf("%d ", i);
+  }
+  return 0;
+}
---
+++ C Language (with C++ Version)/C Flow Control (with C++)/C for Loop/for loop.cpp
@@ -0,0 +1,14 @@
+/* for loop */
+// Print numbers from 1 to 10
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+  for (i = 1; i < 11; ++i)
+  {
+    cout << " " << i;
+  }
+  return 0;
+}
