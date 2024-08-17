ff3b97fe2385b9e3f7012dfff245b7d4e80778b8
file00 <51076009+file00@users.noreply.github.com>
2024-08-17 11:27:42 +0900

    Update Array Input & Output.c

--- C Language (with C++ Version)/C Arrays (with C++)/Array Input & Output.c
+++ C Language (with C++ Version)/C Arrays (with C++)/Array Input & Output.c
@@ -2,6 +2,7 @@
 // Program to take 5 values from the user and store them in an array
 // Print the elements stored in the array
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
