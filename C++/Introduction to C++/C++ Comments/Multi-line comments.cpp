f5e756434ebc67f041c14853012119d58d40d929
file00 <51076009+file00@users.noreply.github.com>
2024-08-18 23:58:03 +0900

    Add files via upload

---
+++ C++/Introduction to C++/C++ Comments/Multi-line comments.cpp
@@ -0,0 +1,15 @@
+/* Multi-line comments */
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* declaring a variable
+    to store salary to employees
+    */
+    int salary = 2000;
+    
+    // print the salary
+    cout << "Salary: "<< salary;
+
+    return 0;
+}
