038f0cc701f5547f96b145043f79db7168c65a5c
file00 <51076009+file00@users.noreply.github.com>
2024-08-25 15:39:49 +0900

    Add files via upload

---
+++ C++/Flow Control/C++ for Loop/Printing Numbers From 1 to 5.cpp
@@ -0,0 +1,10 @@
+/* Printing Numbers From 1 to 5 */
+#include <iostream>
+using namespace std;
+
+int main() {
+        for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+    }
+    return 0;
+}
