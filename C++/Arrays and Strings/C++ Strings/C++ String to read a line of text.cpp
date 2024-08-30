0e690026f0e339b04938e75362bf79b3096abd0e
file00 <51076009+file00@users.noreply.github.com>
2024-08-30 10:29:27 +0900

    Add files via upload

---
+++ C++/Arrays and Strings/C++ Strings/C++ String to read a line of text.cpp
@@ -0,0 +1,12 @@
+/* C++ String to read a line of text */
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.get(str, 100);
+
+    cout << "You entered: " << str << endl;
+    return 0;
+}
---
+++ C++/Arrays and Strings/C++ Strings/C++ string using string data type.cpp
@@ -0,0 +1,13 @@
+/* C++ string using string data type */
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring a string object
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "You entered: " << str << endl;
+    return 0;
+}
