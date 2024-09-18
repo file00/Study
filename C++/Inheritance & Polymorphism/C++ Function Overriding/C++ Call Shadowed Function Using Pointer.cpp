d879e775ae0c534fe6ffdaecf66f6f45e8250b9c
file00 <51076009+file00@users.noreply.github.com>
2024-09-18 10:02:40 +0900

    Add files via upload

---
+++ C++/Inheritance & Polymorphism/C++ Function Overriding/C++ Call Shadowed Function Using Pointer.cpp
@@ -0,0 +1,32 @@
+/* C++ Call Shadowed Function Using Pointer */
+// C++ program to access shadowed function using pointer
+// of Base type that points to an object of Derived class
+
+#include <iostream>
+using namespace std;
+
+class Base {
+   public:
+    void print() {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base {
+   public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main() {
+    Derived derived1;
+
+    // pointer of Base type that points to derived1
+    Base* ptr = &derived1;
+
+    // call function of Base class using ptr
+    ptr->print();
+
+    return 0;
+}
