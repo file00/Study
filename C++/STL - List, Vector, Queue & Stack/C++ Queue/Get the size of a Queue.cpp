d1dbd2c7eb07e9d576223ed9f4d31d4e2d3eb215
file00 <51076009+file00@users.noreply.github.com>
2024-09-27 09:15:03 +0900

    Add files via upload

---
+++ C++/STL - List, Vector, Queue & Stack/C++ Queue/Get the size of a Queue.cpp
@@ -0,0 +1,21 @@
+/* Get the size of a Queue */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+  // create a queue of string
+  queue<string> languages;
+
+  // push element into the queue
+  languages.push("Python");
+  languages.push("C++");
+  languages.push("Java");
+  
+  // get the size of the queue
+  int size = languages.size();
+  cout << "Size of the queue: " << size;
+
+  return 0;
+}
