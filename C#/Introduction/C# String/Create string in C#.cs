5837d033ae39e86b1567dbc753484143afeabd88
file00 <51076009+file00@users.noreply.github.com>
2024-12-04 20:47:35 +0900

    Add files via upload

---
+++ C#/Introduction/C# String/Create string in C#.cs
@@ -0,0 +1,18 @@
+/* Create string in C# */
+using System;  
+namespace CsharpString {  
+  class Test {
+    public static void Main(string [] args) {
+      
+      // create string
+      string str1 = "C# Programming";
+      string str2 = "Programiz";
+      
+      // print string
+      Console.WriteLine(str1);
+      Console.WriteLine(str2);
+
+      Console.ReadLine();
+    }
+  } 
+}
