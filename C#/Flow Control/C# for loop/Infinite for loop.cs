bd9f37b3b4da1f44a468a07463ceeafe1e85a0fd
file00 <51076009+file00@users.noreply.github.com>
2025-01-17 06:57:19 +0900

    Add files via upload

---
+++ C#/Flow Control/C# for loop/Infinite for loop.cs
@@ -0,0 +1,17 @@
+/* Infinite for loop */
+
+using System;
+
+namespace Loop
+{
+	class ForLoop
+	{
+		public static void Main(string[] args)
+		{
+			for (int i=1 ; i>0; i++)
+			{
+				Console.WriteLine("C# For Loop: Iteration {0}", i);
+			}
+		}
+	}
+}
