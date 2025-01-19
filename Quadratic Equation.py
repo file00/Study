a766901bdc6ba54ee5f2c1e795b8ce469e5065a2
file00 <51076009+file00@users.noreply.github.com>
2025-01-19 09:09:00 +0900

    Delete Quadratic Equation.py

--- Quadratic Equation.py
+++
@@ -1,9 +0,0 @@
-# Find the solution of the quadratic equation 3x^2 - 2x - 1 = 0.
-
-a = 3
-b = -2
-c = -1
-
-x1 = (-b + ((b ** 2 - 4 * a * c)) ** (0.5)) / (2 * a)
-x2 = (-b - ((b ** 2 - 4 * a * c)) ** (0.5)) / (2 * a)
-print('Answers = ', x1, x2)
