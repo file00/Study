97a4dddb05d9b424b599a7648928d97a9fc9887d
file00 <51076009+file00@users.noreply.github.com>
2025-01-19 09:10:39 +0900

    Delete Remaining Theorem.py

--- Remaining Theorem.py
+++
@@ -1,6 +0,0 @@
-# Find the remainder of the polynomial of x 2x^3 - x^2 -4x +12 divided by (x - 3).
-# Remaining Theorem: The remainder of the polynomial f(x) of x divided by the linear expression (x - a) of x is equal to f(a).
-
-x = 3
-a = (2 * (x ** 3)) - (x ** 2) - (4 * x) + 12
-print('Answer = ', a)
