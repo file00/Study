fb4a416c7eb6936947b9fe49ccc2f3ced0d5b1e3
file00 <51076009+file00@users.noreply.github.com>
2025-01-24 18:18:27 +0900

    Add files via upload

---
+++ C#/Flow Control/C# while and do while loop/while Loop.cs
@@ -0,0 +1,19 @@
+/* while Loop */
+
+using System;
+
+namespace Loop
+{
+	class WhileLoop
+	{
+		public static void Main(string[] args)
+		{
+			int i=1;
+			while (i<=5)
+			{
+				Console.WriteLine("C# For Loop: Iteration {0}", i);
+				i++;
+			}
+		}
+	}
+}
