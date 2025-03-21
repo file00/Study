f91aabde2d998e1246519ab382ee50b95c9af7e4
file00 <51076009+file00@users.noreply.github.com>
2025-03-22 07:23:42 +0900

    Add files via upload

---
+++ Python/Python Introduction/Python Comments/Prevent Executing Code Using Comments.py
@@ -0,0 +1,12 @@
+# Prevent Executing Code Using Comments
+
+number1 = 10
+number2 = 15
+
+sum = number1 + number2
+
+print("The sum is:\n", sum)
+print('The product is:\n', product) # Error!!
+
+print("The sum is:", sum)
+# print('The product is:', product)
