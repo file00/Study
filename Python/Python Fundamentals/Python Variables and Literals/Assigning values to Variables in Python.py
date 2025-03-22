ca56bd7662e5abf1a781b5d6fdda202f52c29c09
file00 <51076009+file00@users.noreply.github.com>
2025-03-23 08:22:50 +0900

    Add files via upload

---
+++ Python/Python Fundamentals/Python Variables and Literals/Assigning values to Variables in Python.py
@@ -0,0 +1,8 @@
+# Assigning values to Variables in Python
+
+# assign value to site_name variable
+site_name = 'programiz.pro'
+
+print(site_name)
+
+# Output: programiz.pro
---
+++ Python/Python Fundamentals/Python Variables and Literals/Changing the Value of a Variable in Python.py
@@ -0,0 +1,9 @@
+# Changing the Value of a Variable in Python
+
+site_name = 'programiz.pro'
+print(site_name)
+
+# assigning a new value to site_name
+site_name = 'apple.com'
+
+print(site_name)
