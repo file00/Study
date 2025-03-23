eb2aa7a80c43ac87a30e77356bf636c88c89146e
file00 <51076009+file00@users.noreply.github.com>
2025-03-24 07:28:15 +0900

    Add files via upload

---
+++ Python/Python Fundamentals/Python Type Conversion/Converting integer to float.py
@@ -0,0 +1,10 @@
+# Converting integer to float
+
+integer_number = 123
+float_number = 1.23
+
+new_number = integer_number + float_number
+
+# display new value and resulting data type
+print("Value:",new_number)
+print("Data Type:",type(new_number))
