bfb3b2f6596455bb5e094a2c856bd874d2c3b16e
file00 <51076009+file00@users.noreply.github.com>
2025-04-01 08:54:58 +0900

    Add files via upload

---
+++ Python/Python Fundamentals/Python Operators/Logical Operators.py
@@ -0,0 +1,11 @@
+# Logical Operators
+
+# logical AND
+print(True and True)     # True
+print(True and False)    # False
+
+# logical OR
+print(True or False)     # True
+
+# logical NOT
+print(not True)          # False
