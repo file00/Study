75bd228b5e87774e4e138145ea42c0e785deebf5
file00 <51076009+file00@users.noreply.github.com>
2025-04-04 06:20:08 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python if else Statement/Python if Statement.py
@@ -0,0 +1,9 @@
+# Python if Statement
+
+number = int(input('Enter a number: '))
+
+# check if number is greater than 0
+if number > 0:
+    print(f'{number} is a positive number.')
+
+print('A statement outside the if statement.')
