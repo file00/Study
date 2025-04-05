443192d2aec3daf73043880fb799eab1ccdc4853
file00 <51076009+file00@users.noreply.github.com>
2025-04-06 06:42:25 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python if else Statement/Python if elif else Statement.py
@@ -0,0 +1,14 @@
+# Python if…elif…else Statement
+
+number = -5
+
+if number > 0:
+    print('Positive number')
+
+elif number < 0:
+    print('Negative number')
+
+else:
+    print('Zero')
+
+print('This statement is always executed')
