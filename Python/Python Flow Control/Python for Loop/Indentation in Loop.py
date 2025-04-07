fdab9546e23c31df11bf9b11a8aaa64d6d1a55ad
file00 <51076009+file00@users.noreply.github.com>
2025-04-08 06:09:25 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python for Loop/Indentation in Loop.py
@@ -0,0 +1,11 @@
+# Indentation in Loop
+
+languages = ['Swift', 'Python', 'Go']
+
+# start of the loop
+for lang in languages:
+    print(lang)
+    print('-----')
+# end of the for loop
+
+print('Last statement')
---
+++ Python/Python Flow Control/Python for Loop/Loop Through a String.py
@@ -0,0 +1,7 @@
+# Loop Through a String
+
+language = 'Python'
+
+# iterate over each character in language
+for x in language:
+    print(x)
