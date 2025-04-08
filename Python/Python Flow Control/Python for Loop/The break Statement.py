795bee7d51fdec9e8c42dbba82d65c96ae8a56ef
file00 <51076009+file00@users.noreply.github.com>
2025-04-09 08:17:38 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python for Loop/The break Statement.py
@@ -0,0 +1,8 @@
+# The break Statement
+
+languages = ['Swift', 'Python', 'Go', 'C++']
+
+for lang in languages:
+    if lang == 'Go':
+        break
+    print(lang)
---
+++ Python/Python Flow Control/Python for Loop/for Loop with Python range().py
@@ -0,0 +1,5 @@
+# for Loop with Python range()
+
+# iterate from i = 0 to i = 3
+for i in range(0, 4):
+    print(i)
