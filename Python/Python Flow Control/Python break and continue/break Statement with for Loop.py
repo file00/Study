db27c83c829383bd4dd16e9871cbe5ec61bb6f1a
file00 <51076009+file00@users.noreply.github.com>
2025-04-14 08:56:17 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python break and continue/break Statement with for Loop.py
@@ -0,0 +1,6 @@
+# break Statement with for Loop
+
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
---
+++ Python/Python Flow Control/Python break and continue/continue Statement with for Loop.py
@@ -0,0 +1,6 @@
+# continue Statement with for Loop
+
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
