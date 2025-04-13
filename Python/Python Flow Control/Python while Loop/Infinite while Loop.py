8ce8ed1a32cabab12dcc0fb3ef6e03077df16f00
file00 <51076009+file00@users.noreply.github.com>
2025-04-13 10:02:39 +0900

    Add files via upload

---
+++ Python/Python Flow Control/Python while Loop/Infinite while Loop.py
@@ -0,0 +1,7 @@
+# Infinite while Loop
+
+age = 32
+    
+# the test condition is always True
+while True:
+    print('You can vote')
---
+++ Python/Python Flow Control/Python while Loop/Python while Loop.py
@@ -0,0 +1,11 @@
+# Python while Loop
+
+# Print numbers until the user enters 0
+number = int(input('Enter a number: '))
+
+# iterate until the user enters 0
+while number != 0:
+    print(f'You entered {number}.')
+    number = int(input('Enter a number: '))
+
+print('The end.')
