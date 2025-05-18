037f1807e79468fb6f206eb4298324ddf1132f5a
file00 <51076009+file00@users.noreply.github.com>
2025-05-18 16:50:23 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Programming Files (with C++)/C File Handling (with C++)/Read from a Text File.c
@@ -0,0 +1,25 @@
+/* Read from a Text File */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num;
+   FILE *fptr;
+
+   if ((fptr = fopen("C:\\program.txt","r")) == NULL){
+       printf("Error! opening file");
+
+       // Program exits if the file pointer returns NULL.
+       exit(1);
+   }
+
+   fscanf(fptr,"%d", &num);
+
+   printf("Value of n=%d", num);
+   fclose(fptr); 
+  
+   return 0;
+}
