d13c478bee77bcf3ddefff0f7d369aa90c116d32
file00 <51076009+file00@users.noreply.github.com>
2025-05-20 12:20:19 +0900

    Delete C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (C++ Version).cpp

--- C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (C++ Version).cpp
+++
@@ -1,9 +0,0 @@
-/* Your First C Program (C++ Version) */
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl; // print Hello World to the screen
-
-    return 0;
-}
