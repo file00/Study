23b44af34ce363e85ffd831a8ce6b708218d6c74
file00 <51076009+file00@users.noreply.github.com>
2025-06-20 00:19:17 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments about C++.cpp
@@ -0,0 +1,15 @@
+/* Prevent Executing Code Using Comments about C++ */ 
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    int number1 = 10;
+    int number2 = 15;
+    int sum = number1 + number2;
+ 
+    // print the age variable
+    cout << "The sum is: " << sum;
+
+    return 0;
+}
