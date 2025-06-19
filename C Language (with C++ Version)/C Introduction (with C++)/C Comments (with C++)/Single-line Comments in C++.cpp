0defafacf77e8a53f1c52042ebd0e79f4b545bef
file00 <51076009+file00@users.noreply.github.com>
2025-06-20 00:13:31 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Single-line Comments in C++.cpp
@@ -0,0 +1,14 @@
+/* Single-line Comments in C++ */
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    // create integer variable    
+    int age = 25;
+    
+    // print the age variable
+    cout << "Age: " << age;
+
+    return 0;
+}
