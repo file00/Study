1d9226e51352cfb55e5a44ea29e4a04c30d90692
file00 <51076009+file00@users.noreply.github.com>
2025-06-23 05:23:17 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Numbers, Type Conversion and Mathematics/Python Random Module.py
@@ -0,0 +1,19 @@
+# Python Random Module
+
+import random
+
+print(random.randrange(10, 20))
+
+list1 = ['a', 'b', 'c', 'd', 'e']
+
+# get random item from list1
+print(random.choice(list1))
+
+# Shuffle list1
+random.shuffle(list1)
+
+# Print the shuffled list1
+print(list1)
+
+# Print random element
+print(random.random())
