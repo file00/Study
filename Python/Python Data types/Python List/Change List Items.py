d2ab8cf51a459205916af60bec7f1db4c772f342
file00 <51076009+file00@users.noreply.github.com>
2025-06-29 00:02:21 +0900

    Add files via upload

---
+++ Python/Python Data types/Python List/Change List Items.py
@@ -0,0 +1,12 @@
+# Change List Items
+
+colors = ['Red', 'Black', 'Green']
+print('Original List:', colors)
+
+# change the first item to 'Purple'
+colors[2] = 'Purple'
+
+# change the third item to 'Blue'
+colors[2] = 'Blue'
+
+print('Updated List:', colors)
---
+++ Python/Python Data types/Python List/Remove an Item From a List.py
@@ -0,0 +1,8 @@
+# Remove an Item From a List
+
+numbers = [2,4,7,9]
+
+# remove 4 from the list
+numbers.remove(4)
+
+print(numbers) 
