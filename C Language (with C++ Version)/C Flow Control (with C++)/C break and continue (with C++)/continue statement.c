31db2ebc9f937f80bf5cc24ed14df7d588c0b9bb
file00 <51076009+file00@users.noreply.github.com>
2025-07-12 07:59:21 +0900

    Update continue statement.c

--- C Language (with C++ Version)/C Flow Control (with C++)/C break and continue (with C++)/continue statement.c
+++ C Language (with C++ Version)/C Flow Control (with C++)/C break and continue (with C++)/continue statement.c
@@ -1,4 +1,4 @@
-/* continue statement */
+// continue statement 
 // Program to calculate the sum of numbers (10 numbers max)
 // If the user enters a negative number, it's not added to the result
 
