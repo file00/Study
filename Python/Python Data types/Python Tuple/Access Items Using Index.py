13953f6e8ca173696b4e092d514e514b8a17787a
file00 <51076009+file00@users.noreply.github.com>
2025-07-14 05:38:12 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Tuple/Access Items Using Index.py
@@ -0,0 +1,9 @@
+# Access Items Using Index
+
+languages = ('Python', 'Swift', 'C++')
+
+# access the first item
+print(languages[0])   # Python
+
+# access the third item
+print(languages[2])   # C++
---
+++ Python/Python Data types/Python Tuple/Tuple Cannot be Modified.py
@@ -0,0 +1,8 @@
+# Tuple Cannot be Modified
+
+cars = ('BMW', 'Tesla', 'Ford', 'Toyota')
+
+# trying to modify a tuple
+cars[0] = 'Nissan'    # error
+       
+print(cars)
