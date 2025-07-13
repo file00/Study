0839358b16edc07e68b0e189848e5fb64fac9d9f
file00 <51076009+file00@users.noreply.github.com>
2025-07-14 05:41:04 +0900

    Update goto Statement.c

--- C Language (with C++ Version)/C Flow Control (with C++)/C goto Statement (without C++)/goto Statement.c
+++ C Language (with C++ Version)/C Flow Control (with C++)/C goto Statement (without C++)/goto Statement.c
@@ -1,4 +1,4 @@
-/* goto Statement */
+// goto Statement 
 // Program to calculate the sum and average of positive numbers
 // If the user enters a negative number, the sum and average are displayed.
 
