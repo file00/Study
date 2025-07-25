0532638090d9046b8835f34ab51eb6a1b650e238
file00 <51076009+file00@users.noreply.github.com>
2025-07-26 07:15:17 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Strings/Compare Two Strings.py
@@ -0,0 +1,11 @@
+# Compare Two Strings
+
+str1 = "Hello, world!"
+str2 = "I love Swift."
+str3 = "Hello, world!"
+
+# compare str1 and str2
+print(str1 == str2)
+
+# compare str1 and str3
+print(str1 == str3)
---
+++ Python/Python Data types/Python Strings/Join Two or More Strings.py
@@ -0,0 +1,10 @@
+# Join Two or More Strings
+
+greet = "Hello, "
+name = "Jack"
+
+# using + operator
+result = greet + name
+print(result)
+
+# Output: Hello, Jack
