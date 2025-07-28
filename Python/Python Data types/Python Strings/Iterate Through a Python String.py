715f15ab2872516727b636ef715a8b5835f01d75
file00 <51076009+file00@users.noreply.github.com>
2025-07-28 15:24:38 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Strings/Iterate Through a Python String.py
@@ -0,0 +1,6 @@
+# Iterate Through a Python String
+greet = 'Hello'
+
+# iterating through greet string
+for letter in greet:
+    print(letter)
---
+++ Python/Python Data types/Python Strings/Python String Length.py
@@ -0,0 +1,7 @@
+# Python String Length
+greet = 'Hello'
+
+# count length of greet string
+print(len(greet))
+
+# Output: 5
