230a4c5b6e32591037cfd85ecf2521261182a062
file00 <51076009+file00@users.noreply.github.com>
2025-07-30 07:17:56 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Sets/Create an Empty Set in Python.py
@@ -0,0 +1,13 @@
+# Create an Empty Set in Python
+
+# create an empty set
+empty_set = set()
+
+# create an empty dictionary
+empty_dictionary = { }
+
+# check data type of empty_set
+print('Data type of empty_set:', type(empty_set))
+
+# check data type of dictionary_set
+print('Data type of empty_dictionary:', type(empty_dictionary))
