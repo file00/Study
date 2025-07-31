a4b8faabc236dd029af495bae73da6d7188a660b
file00 <51076009+file00@users.noreply.github.com>
2025-08-01 08:52:40 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Sets/Add Items to a Set in Python.py
@@ -0,0 +1,10 @@
+# Add Items to a Set in Python
+
+numbers = {21, 34, 54, 12}
+
+print('Initial Set:',numbers)
+
+# using add() method
+numbers.add(32)
+
+print('Updated Set:', numbers) 
---
+++ Python/Python Data types/Python Sets/Update Python Set.py
@@ -0,0 +1,11 @@
+# Update Python Set
+
+companies = {'Lacoste', 'Ralph Lauren'}
+tech_companies = ['apple', 'google', 'apple']
+
+# using update() method
+companies.update(tech_companies)
+
+print(companies)
+
+# Output: {'google', 'apple', 'Lacoste', 'Ralph Lauren'}
