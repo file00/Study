8fcd4ec486303c8161c8c0bc289c97edac885999
file00 <51076009+file00@users.noreply.github.com>
2025-08-02 00:25:33 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Sets/Remove an Element from a Set.py
@@ -0,0 +1,10 @@
+# Remove an Element from a Set
+
+languages = {'Swift', 'Java', 'Python'}
+
+print('Initial Set:',languages)
+
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+
+print('Set after remove():', languages)
