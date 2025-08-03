4db6e85e8f8128897e5da3b9422b85a21647fb36
file00 <51076009+file00@users.noreply.github.com>
2025-08-04 07:56:25 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Sets/Set Intersection.py
@@ -0,0 +1,12 @@
+# Set Intersection
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {1, 2, 3}
+
+# perform intersection operation using &
+print('Intersection using &:', A & B)
+
+# perform intersection operation using intersection()
+print('Intersection using intersection():', A.intersection(B)) 
