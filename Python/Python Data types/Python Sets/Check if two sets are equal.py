122529e03439b777ff03a7f7e697efda209b2ed2
file00 <51076009+file00@users.noreply.github.com>
2025-08-05 05:40:34 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Sets/Check if two sets are equal.py
@@ -0,0 +1,13 @@
+# Check if two sets are equal
+
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {3, 5, 1}
+
+# perform difference operation using &
+if A == B:
+    print('Set A and Set B are equal')
+else:
+    print('Set A and Set B are not equal')
