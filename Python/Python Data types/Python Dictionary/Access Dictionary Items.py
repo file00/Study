bd3af01db285f608689c863eb4cdb5a1599c765b
file00 <51076009+file00@users.noreply.github.com>
2025-08-07 00:02:46 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Dictionary/Access Dictionary Items.py
@@ -0,0 +1,11 @@
+# Access Dictionary Items
+
+country_capitals = {
+  "Germany": "Berlin", 
+  "Canada": "Ottawa", 
+  "England": "London"
+}
+
+# access the value of keys
+print(country_capitals["Germany"])    # Output: Berlin
+print(country_capitals["England"])    # Output: London
---
+++ Python/Python Data types/Python Dictionary/Add Items to a Dictionary.py
@@ -0,0 +1,11 @@
+# Add Items to a Dictionary
+
+country_capitals = {
+  "Germany": "Berlin", 
+  "Canada": "Ottawa", 
+}
+
+# add an item with "Italy" as key and "Rome" as its value
+country_capitals["Italy"] = "Rome"
+
+print(country_capitals)
