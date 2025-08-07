5f0c3fed22b65f83c829b6f824ea15a3f5baff49
file00 <51076009+file00@users.noreply.github.com>
2025-08-08 00:31:55 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Dictionary/Remove Dictionary Items.py
@@ -0,0 +1,11 @@
+# Remove Dictionary Items
+
+country_capitals = {
+  "Germany": "Berlin", 
+  "Canada": "Ottawa", 
+}
+
+# delete item having "Germany" key
+del country_capitals["Germany"]
+
+print(country_capitals)
