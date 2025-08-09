93026cec57b89bbbb292a18d086feb3b1ee5a001
file00 <51076009+file00@users.noreply.github.com>
2025-08-10 00:22:23 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Variables.cpp
@@ -0,0 +1,16 @@
+// Variables 
+using namespace std;
+#include <iostream>
+
+int main() {
+    int age = 25;
+    char ch = 'a';
+    
+    cout << ch << '\n';
+    cout << age << '\n';
+    
+    ch = 'l';
+    cout << ch;
+
+    return 0;
+}
