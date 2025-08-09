54fe151e3258291d03856b52b20ea357d8f1663e
file00 <51076009+file00@users.noreply.github.com>
2025-08-10 00:19:58 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Dictionary/Dictionary Membership Test.py
@@ -0,0 +1,12 @@
+# Dictionary Membership Test
+
+file_types = {
+    ".txt": "Text File",
+    ".pdf": "PDF Document",
+    ".jpg": "JPEG Image",
+}
+
+# use of in and not in operators
+print(".pdf" in file_types)       # Output: True
+print(".mp3" in file_types)       # Output: False
+print(".mp3" not in file_types)   # Output: True
---
+++ Python/Python Data types/Python Dictionary/Find Dictionary Length.py
@@ -0,0 +1,16 @@
+# Find Dictionary Length
+
+country_capitals = {"England": "London", "Italy": "Rome"}
+
+# get dictionary's length
+print(len(country_capitals))   # Output: 2
+
+numbers = {10: "ten", 20: "twenty", 30: "thirty"}
+
+# get dictionary's length
+print(len(numbers))            # Output: 3
+
+countries = {}
+
+# get dictionary's length
+print(len(countries))          # Output: 0
