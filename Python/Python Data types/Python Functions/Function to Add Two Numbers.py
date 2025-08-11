dfc02fbe21fa81329d0e5e534711da3851bc7b28
file00 <51076009+file00@users.noreply.github.com>
2025-08-11 11:05:12 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Functions/Function to Add Two Numbers.py
@@ -0,0 +1,9 @@
+# Function to Add Two Numbers
+
+# function with two arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum: ", sum)
+
+# function call with two values
+add_numbers(5, 4)
---
+++ Python/Python Data types/Python Functions/Python Function Call.py
@@ -0,0 +1,9 @@
+# Python Function Call
+
+def greet():
+    print('Hello World!')
+
+# call the function
+greet()
+
+print('Outside function')
