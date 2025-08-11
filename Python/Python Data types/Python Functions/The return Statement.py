e780368037b16e158b6e251494619def535514e3
file00 <51076009+file00@users.noreply.github.com>
2025-08-12 00:10:58 +0900

    Add files via upload

---
+++ Python/Python Data types/Python Functions/The return Statement.py
@@ -0,0 +1,11 @@
+# The return Statement
+
+# function definition
+def find_square(num):
+    result = num * num
+    return result
+
+# function call
+square = find_square(3)
+
+print('Square:', square)
