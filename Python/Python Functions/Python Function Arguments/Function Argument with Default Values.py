63db1547b58c03d7a0021a1da2db56b03a9b2822
file00 <51076009+file00@users.noreply.github.com>
2025-08-14 00:18:08 +0900

    Add files via upload

---
+++ Python/Python Functions/Python Function Arguments/Function Argument with Default Values.py
@@ -0,0 +1,15 @@
+# Function Argument with Default Values
+
+def add_numbers( a = 7,  b = 8):
+    sum = a + b
+    print('Sum:', sum)
+
+
+# function call with two arguments
+add_numbers(2, 3)
+
+#  function call with one argument
+add_numbers(a = 2)
+
+# function call with no arguments
+add_numbers()
---
+++ Python/Python Functions/Python Function Arguments/Python Function Arguments.py
@@ -0,0 +1,9 @@
+# Python Function Arguments
+
+def add_numbers(a, b):
+    sum = a + b
+    print('Sum:', sum)
+
+add_numbers(2, 3)
+
+# Output: Sum: 5
