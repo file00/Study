7b075fe0fff3d6a3b8b352120ffff345628fe7a9
file00 <51076009+file00@users.noreply.github.com>
2025-08-15 00:03:24 +0900

    Add files via upload

---
+++ Python/Python Functions/Python Function Arguments/Python Keyword Argument.py
@@ -0,0 +1,7 @@
+# Python Keyword Argument
+
+def display_info(first_name, last_name):
+    print('First Name:', first_name)
+    print('Last Name:', last_name)
+
+display_info(last_name = 'Cartman', first_name = 'Eric')
