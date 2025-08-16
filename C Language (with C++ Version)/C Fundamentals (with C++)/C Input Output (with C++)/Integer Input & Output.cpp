b82fd17d3f26f892f8a7ce3f1b96e716a6aa8b98
file00 <51076009+file00@users.noreply.github.com>
2025-08-17 06:42:46 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Input & Output.cpp
@@ -0,0 +1,11 @@
+// Integer Input/Output
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testInteger;
+    cout << "Enter an integer: ";
+    cin >> testInteger;
+    cout << "Number = " << testInteger;
+    return 0;
+}
