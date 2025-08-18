dfb4b56fff487d7247739e9ca8c66d174049fd9e
file00 <51076009+file00@users.noreply.github.com>
2025-08-19 06:01:03 +0900

    Add files via upload

---
+++ Python/Python Functions/Python Global Keyword/Access and Modify Python Global Variable.py
@@ -0,0 +1,22 @@
+# Access and Modify Python Global Variable
+
+c = 1 # global variable
+
+def add():
+    print(c)
+
+add()
+
+# Output: 1
+
+# global variable (Error!!)
+# c = 1 
+
+# def add():
+
+#      # increment c by 2
+#     c = c + 2
+
+#     print(c)
+
+# add()
---
+++ Python/Python Functions/Python Global Keyword/Changing Global Variable From Inside a Function using global.py
@@ -0,0 +1,18 @@
+# Changing Global Variable From Inside a Function using global
+
+# global variable
+c = 1 
+
+def add():
+
+    # use of global keyword
+    global c
+
+    # increment c by 2
+    c = c + 2 
+
+    print(c)
+
+add()
+
+# Output: 3 
