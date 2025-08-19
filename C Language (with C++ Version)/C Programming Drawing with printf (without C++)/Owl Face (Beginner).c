3e29453f4b4d0312e2909d38bec44886e91d44a9
file00 <51076009+file00@users.noreply.github.com>
2025-08-20 00:03:17 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Programming Drawing with printf (without C++)/Owl Face (Beginner).c
@@ -0,0 +1,10 @@
+// Owl Face (Beginner)
+#include <stdio.h>
+
+int main() {
+    printf("  /\\___/\\  \n");
+    printf(" (  o o  ) \n");
+    printf(" (   V   ) \n");
+    printf("  \\_____/  \n");
+    return 0;
+}
