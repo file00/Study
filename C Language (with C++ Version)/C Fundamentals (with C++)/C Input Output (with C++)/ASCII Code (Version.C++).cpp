222e397b0c9637d31cf073b155486d030862ed5a
file00 <51076009+file00@users.noreply.github.com>
2025-08-24 10:04:31 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Code (Version.C++).cpp
@@ -0,0 +1,13 @@
+// ASCII Code (Version.C++)
+#include <iostream>
+using namespace std;
+
+int main() {
+    char chr;
+    cout << "Enter a character: ";
+    cin >> chr;
+
+    cout << "You entered " << chr << "." << endl;
+    cout << "ASCII value is " << static_cast<int>(chr) << ".";
+    return 0;
+}
