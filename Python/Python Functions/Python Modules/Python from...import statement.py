2a32d6a8a6bc23ba00e642c9eab6395db86d4f9e
file00 <51076009+file00@users.noreply.github.com>
2025-08-25 06:57:16 +0900

    Add files via upload

---
+++ Python/Python Functions/Python Modules/Python from...import statement.py
@@ -0,0 +1,8 @@
+# Python from...import statement
+
+# import only pi from math module
+from math import pi
+
+print(pi)
+
+# Output: 3.141592653589793
---
+++ Python/Python Functions/Python Modules/Python import with Renaming.py
@@ -0,0 +1,8 @@
+# Python import with Renaming
+
+# import module by renaming it
+import math as m
+
+print(m.pi)
+
+# Output: 3.141592653589793
