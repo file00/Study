dec13c63c9323e058ac9e518409f72ffd29707af
file00 <51076009+file00@users.noreply.github.com>
2025-08-27 00:06:14 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Programming Arrays (with C++)/C Arrays (with C++)/Access Array Elements.c
@@ -0,0 +1,16 @@
+// Access Array Elements
+
+#include <stdio.h>
+
+int main()
+{
+    int mark[] = {19, 10, 8, 17, 9};
+    
+    printf("%d\n", mark[0]);
+    printf("%d\n", mark[1]);
+    printf("%d\n", mark[2]);
+    printf("%d\n", mark[3]);
+    printf("%d", mark[4]);
+
+    return 0;
+}
