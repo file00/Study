b8be58d28a63b3013eb59b80391c2fd35bdd5d9f
file00 <51076009+file00@users.noreply.github.com>
2025-09-14 00:06:57 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Pointers (without C++)/C Array and Pointer Examples (without C++)/C Program to Access Array Elements Using Pointer (without C++)/Access Array Elements Using Pointers.c
@@ -0,0 +1,15 @@
+// Access Array Elements Using Pointers
+
+#include <stdio.h>
+int main() {
+    int data[5];
+
+    printf("Enter elements: ");
+    for (int i = 0; i < 5; ++i)
+        scanf("%d", data + i);
+
+    printf("You entered: \n");
+    for (int i = 0; i < 5; ++i)
+        printf("%d\n", *(data + i));
+    return 0;
+}
