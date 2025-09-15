20b09743385ef906906e5c1292c1506d669f8a99
file00 <51076009+file00@users.noreply.github.com>
2025-09-16 00:03:09 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Strings (with C++)/C Programming Strings (with C++)/scanf() to read a string.c
@@ -0,0 +1,12 @@
+// scanf() to read a string 
+
+#include <stdio.h>
+
+int main()
+{
+    char name[20];
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("Your name is %s.", name);
+    return 0;
+}
