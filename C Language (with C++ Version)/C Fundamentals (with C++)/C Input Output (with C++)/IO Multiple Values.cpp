671717c56c95cb213899907480ef4be8d46e7ecd
file00 <51076009+file00@users.noreply.github.com>
2025-09-19 06:12:23 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/IO Multiple Values.cpp
@@ -0,0 +1,18 @@
+// I/O Multiple Values 
+#include <iostream>
+#include <iomanip>  // for setprecision 
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+
+    cout << "Enter integer and then a float: ";
+
+    // Taking multiple inputs
+    cin >> a >> b;
+
+    cout << "You entered " << a << " and " << fixed << setprecision(6) << b;
+    return 0;
+}
