56d9ec97534db4920fe11ec2403df1b2ef8934b4
file00 <51076009+file00@users.noreply.github.com>
2025-09-20 00:11:30 +0900

    Add files via upload

---
+++ Python/Python Files/Python Directory and Files Management/List Directories and Files in Python.py
@@ -0,0 +1,29 @@
+# List Directories and Files in Python
+import os
+
+print(os.getcwd())
+C:\Python33
+
+# list all sub-directories
+os.listdir()
+['DLLs',
+'Doc',
+'include',
+'Lib',
+'libs',
+'LICENSE.txt',
+'NEWS.txt',
+'python.exe',
+'pythonw.exe',
+'README.txt',
+'Scripts',
+'tcl',
+'Tools']
+
+os.listdir('G:\\')
+['$RECYCLE.BIN',
+'Movies',
+'Music',
+'Photos',
+'Series',
+'System Volume Information']
