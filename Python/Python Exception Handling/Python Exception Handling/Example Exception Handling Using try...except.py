198a33b7819f2b3e9b640cf44942ee03f4f47edb
file00 <51076009+file00@users.noreply.github.com>
2025-09-21 00:02:09 +0900

    Add files via upload

---
+++ Python/Python Exception Handling/Python Exception Handling/Example Exception Handling Using try...except.py
@@ -0,0 +1,13 @@
+# Example: Exception Handling Using try...except
+
+try:
+    numerator = 10
+    denominator = 0
+
+    result = numerator/denominator
+
+    print(result)
+except:
+    print("Error: Denominator cannot be 0.")
+
+# Output: Error: Denominator cannot be 0. 
---
+++ Python/Python Exception Handling/Python Exceptions/Python Logical Errors (Exceptions).py
@@ -0,0 +1,4 @@
+# Python Logical Errors (Exceptions)
+
+divide_numbers = 7 / 0
+print(divide_numbers)
