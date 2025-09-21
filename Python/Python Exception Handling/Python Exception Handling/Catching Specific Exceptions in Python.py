94832e45f5fdad462f2cb51ee2a49d12a452dc0e
file00 <51076009+file00@users.noreply.github.com>
2025-09-22 06:54:18 +0900

    Add files via upload

---
+++ Python/Python Exception Handling/Python Exception Handling/Catching Specific Exceptions in Python.py
@@ -0,0 +1,14 @@
+# Catching Specific Exceptions in Python
+
+try:
+    
+    even_numbers = [2,4,6,8]
+    print(even_numbers[5])
+
+except ZeroDivisionError:
+    print("Denominator cannot be 0.")
+    
+except IndexError:
+    print("Index Out of Bound.")
+
+# Output: Index Out of Bound
