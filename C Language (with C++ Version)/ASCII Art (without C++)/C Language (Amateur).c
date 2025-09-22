eea17af393315fb1d22a6bd7f5e7b5635d24a8a6
file00 <51076009+file00@users.noreply.github.com>
2025-09-23 00:02:03 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/C Language (Amateur).c
@@ -0,0 +1,12 @@
+// "C Language" ASCII Art in C
+
+#include <stdio.h>
+
+int main() {
+    printf("   CCCCC       L            AAA      N   N   GGGG   U   U   AAAAA   GGGG   EEEEE  \n");
+    printf("  C     C      L           A   A     NN  N  G       U   U  A     A  G      E      \n");
+    printf("  C            L          AAAAAAA    N N N  G  GGG  U   U  AAAAAAA  G  GGG EEEE   \n");
+    printf("  C     C      L          A     A    N  NN  G    G  U   U  A     A  G    G E      \n");
+    printf("   CCCCC       LLLLLLL    A     A    N   N   GGGG    UUU   A     A   GGGG  EEEEE  \n");
+    return 0;
+}
---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Money Symbol (Beginner).c
@@ -0,0 +1,13 @@
+// Money Symbol (Beginner)
+#include <stdio.h>
+
+int main() {
+    printf("    $$$$$    \n");
+    printf("   $     $   \n");
+    printf("   $  |  $   \n");
+    printf("    $$$$$    \n");
+    printf("   $  |      \n");
+    printf("   $     $   \n");
+    printf("    $$$$$    \n");
+    return 0;
+}
