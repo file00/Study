14d0f4dcdddd468feedafe37078b7fb7e6aada15
file00 <51076009+file00@users.noreply.github.com>
2025-09-28 07:15:18 +0900

    Add files via upload

---
+++ Java/Java Introduction/Java Comments/Single-line Comment.java
@@ -0,0 +1,13 @@
+// Single-line Comment
+
+class Main {
+    public static void main(String[] args) {
+        
+        // declare and initialize two variables
+        int a = 1;
+        int b = 3;
+        
+        // print the output
+        System.out.println("This is output");
+    }
+}
