748fe1836a47f4a11721f48903ffd95e06b576ac
file00 <51076009+file00@users.noreply.github.com>
2025-09-29 00:03:49 +0900

    Add files via upload

---
+++ Java/Java Introduction/Java Comments/Multi-line Comment.java
@@ -0,0 +1,12 @@
+// Multi-line Comment
+
+/* This is an example of  multi-line comment.
+ * The program prints "Hello, World!" to the standard output.
+ */
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
