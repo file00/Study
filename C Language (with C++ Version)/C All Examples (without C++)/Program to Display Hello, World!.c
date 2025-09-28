c1fcc440589ff9f725db7f2295a2ab065a79f8fc
file00 <51076009+file00@users.noreply.github.com>
2025-09-29 00:03:34 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C All Examples (without C++)/Program to Display Hello, World!.c
@@ -0,0 +1,8 @@
+// Program to Display "Hello, World!"
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
---
+++ C Language (with C++ Version)/C All Examples (without C++)/Program to Print an Integer.c
@@ -0,0 +1,16 @@
+// Program to Print an Integer
+
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
