a54d6043f6215374f9bbbd687e036c0b138630d0
file00 <51076009+file00@users.noreply.github.com>
2025-10-04 00:05:47 +0900

    Delete Python/Python Files/Python CSV Read and Write CSV Files/Working With CSV Files in Python directory

--- Python/Python Files/Python CSV Read and Write CSV Files/Working With CSV Files in Python/Using Python Pandas to Handle CSV Files/Write to a CSV Files.py
+++
@@ -1,9 +0,0 @@
-# Write to a CSV Files
-
-import pandas as pd
-
-# creating a data frame
-df = pd.DataFrame([['Jack', 24], ['Rose', 22]], columns = ['Name', 'Age'])
-
-# writing data frame to a CSV file
-df.to_csv('person.csv')
