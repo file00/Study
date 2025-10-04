86cef1e2dac0c6c61b5bda49150ddae0c575b803
file00 <51076009+file00@users.noreply.github.com>
2025-10-05 00:32:23 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java boolean data type.java
@@ -0,0 +1,9 @@
+// Java boolean data type
+
+class Main {
+  public static void main(String[] args) {
+    	
+    boolean flag = true;
+    System.out.println(flag);    // prints true
+  }
+}
---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java byte data type.java
@@ -0,0 +1,10 @@
+// Java byte data type
+
+class Main {
+  public static void main(String[] args) {
+
+    byte range;
+    range = 124;
+    System.out.println(range);    // prints 124
+  }
+}
