2087695d32fc239972730d583fc540bd2e9b7fcb
file00 <51076009+file00@users.noreply.github.com>
2025-10-06 00:17:24 +0900

    Delete Python/Python Files/Reading CSV files in Python/Basic Usage of csv.reader()/Read CSV file Having Tab Delimiter.py

--- Python/Python Files/Reading CSV files in Python/Basic Usage of csv.reader()/Read CSV file Having Tab Delimiter.py
+++
@@ -1,6 +0,0 @@
-# Read CSV file Having Tab Delimiter
-import csv
-with open('innovators.csv', 'r') as file:
-    reader = csv.reader(file, delimiter = '\t')
-    for row in reader:
-        print(row)
