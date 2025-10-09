f12e9b9a7e7da8d05cf5184b2870601482d15e90
file00 <51076009+file00@users.noreply.github.com>
2025-10-10 00:34:43 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java int data type.java
@@ -0,0 +1,9 @@
+// Java int data type
+
+class Main {
+  public static void main(String[] args) {
+    	
+    int range = -4250000;
+    System.out.println(range);  // print -4250000
+  }
+}
---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java short data type.java
@@ -0,0 +1,10 @@
+// Java short data type
+
+class Main {
+  public static void main(String[] args) {
+    	
+    short temperature;
+    temperature = -200;
+    System.out.println(temperature);  // prints -200
+  }
+}
