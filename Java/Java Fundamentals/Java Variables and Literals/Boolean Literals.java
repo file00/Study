8f1876d62ba2d35d4bb5c4f2599ddc6167ca0c10
file00 <51076009+file00@users.noreply.github.com>
2025-10-12 00:07:39 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Variables and Literals/Boolean Literals.java
@@ -0,0 +1,12 @@
+// Boolean Literals
+
+class Main {
+  public static void main(String[] args) {
+    	
+    boolean flag1 = false;
+    boolean flag2 = true;
+
+    System.out.println(flag1);  
+    System.out.println(flag2);    
+  }
+}
---
+++ Java/Java Fundamentals/Java Variables and Literals/String Literals.java
@@ -0,0 +1,11 @@
+// String Literals
+class Main {
+  public static void main(String[] args) {
+    	
+    String str1 = "Java Programming";
+    String str2 = "Programiz";
+
+    System.out.println(str1);  // Java Programming
+    System.out.println(str2);    // Programiz
+  }
+}
