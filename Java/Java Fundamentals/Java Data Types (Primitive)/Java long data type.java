ad2026c815beaf6332ea87530ac1498295505e62
file00 <51076009+file00@users.noreply.github.com>
2025-10-12 00:09:11 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java long data type.java
@@ -0,0 +1,8 @@
+// Java long data type
+class Main {
+  public static void main(String[] args) {
+    	
+    long range = -42332200000L;
+    System.out.println(range);    // prints -42332200000
+  }
+}
