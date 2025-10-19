5f2fb7dd1a233ca2ba2f7848a888ac808b218d69
file00 <51076009+file00@users.noreply.github.com>
2025-10-20 06:55:41 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Strings (with C++)/String Examples in C Programming (without C++)/Find the length of a string (without C++)/Calculate Length of String without Using strlen() Function.c
@@ -0,0 +1,12 @@
+// Calculate Length of String without Using strlen() Function
+
+#include <stdio.h>
+int main() {
+    char s[] = "Programming is fun";
+    int i;
+
+    for (i = 0; s[i] != '\0'; ++i);
+    
+    printf("Length of the string: %d", i);
+    return 0;
+}
