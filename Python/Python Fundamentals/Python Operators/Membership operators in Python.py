2283984b210bf021b0b4ec6f513a191e9b915d6e
file00 <51076009+file00@users.noreply.github.com>
2025-10-26 08:09:09 +0900

    Update Membership operators in Python.py

--- Python/Python Fundamentals/Python Operators/Membership operators in Python.py
+++ Python/Python Fundamentals/Python Operators/Membership operators in Python.py
@@ -4,13 +4,13 @@ message = 'Hello world'
 dict1 = {1:'a', 2:'b'}
 
 # check if 'H' is present in message string
-print('H' in message)  # prints True
+print('H' in message)      # prints True
 
 # check if 'hello' is present in message string
-print('hello' not in message)  # prints True
+print('hello' not in message)      # prints True
 
 # check if '1' key is present in dict1
-print(1 in dict1)  # prints True
+print(1 in dict1)      # prints True
 
 # check if 'a' key is present in dict1
-print('a' in dict1)  # prints False
+print('a' in dict1)      # prints False
