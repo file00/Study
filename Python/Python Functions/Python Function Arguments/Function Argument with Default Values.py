c19cfbaea937ebf15d7b8517e1236295d75830f6
file00 <51076009+file00@users.noreply.github.com>
2025-10-27 08:30:12 +0900

    Update Function Argument with Default Values.py

--- Python/Python Functions/Python Function Arguments/Function Argument with Default Values.py
+++ Python/Python Functions/Python Function Arguments/Function Argument with Default Values.py
@@ -7,7 +7,7 @@ def add_numbers( a = 7,  b = 8):
 # function call with two arguments
 add_numbers(2, 3)
 
-#  function call with one argument
+# function call with one argument
 add_numbers(a = 2)
 
 # function call with no arguments
