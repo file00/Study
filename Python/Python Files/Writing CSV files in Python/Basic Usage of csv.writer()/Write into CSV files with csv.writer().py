5dae587be1ec489bb55c6c0c3a3978d0cdb6a32b
file00 <51076009+file00@users.noreply.github.com>
2025-10-28 07:20:03 +0900

    Add files via upload

---
+++ Python/Python Files/Writing CSV files in Python/Basic Usage of csv.writer()/Write into CSV files with csv.writer().py
@@ -0,0 +1,9 @@
+# Write into CSV files with csv.writer()
+
+import csv
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
---
+++ Python/Python Files/Writing CSV files in Python/Basic Usage of csv.writer()/Writing Multiple Rows with writerows().py
@@ -0,0 +1,10 @@
+# Writing Multiple Rows with writerows()
+
+import csv
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('protagonist.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)
