f9fdca0ef4cc03c639c621a2d977899755132346
file00 <51076009+file00@users.noreply.github.com>
2025-10-29 08:09:39 +0900

    Update Example Exception Handling Using try...except.py

--- Python/Python Exception Handling/Python Exception Handling/Example Exception Handling Using try...except.py
+++ Python/Python Exception Handling/Python Exception Handling/Example Exception Handling Using try...except.py
@@ -8,6 +8,4 @@ try:
 
     print(result)
 except:
-    print("Error: Denominator cannot be 0.")
-
-# Output: Error: Denominator cannot be 0. 
+    print("Error: Denominator cannot be 0.")      # Output: Error: Denominator cannot be 0. 
