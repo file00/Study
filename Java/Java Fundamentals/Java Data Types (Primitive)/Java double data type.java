8ad56c4e2c8575f8c7cf1805c9576c6e69714d74
file00 <51076009+file00@users.noreply.github.com>
2025-10-29 07:56:58 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java double data type.java
@@ -0,0 +1,9 @@
+// Java double data type
+
+class Main {
+  public static void main(String[] args) {
+    	
+    double number = -42.3;
+    System.out.println(number);  // prints -42.3
+  }
+}
---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java float data type.java
@@ -0,0 +1,9 @@
+// Java float data type
+
+class Main {
+  public static void main(String[] args) {
+    	
+    float number = -42.3f;
+    System.out.println(number);  // prints -42.3
+  }
+}
