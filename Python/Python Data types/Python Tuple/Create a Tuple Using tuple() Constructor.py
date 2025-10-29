d12ed69fc91673ccb985160f5468ef931616a0ba
file00 <51076009+file00@users.noreply.github.com>
2025-10-30 07:48:41 +0900

    Update Create a Tuple Using tuple() Constructor.py

--- Python/Python Data types/Python Tuple/Create a Tuple Using tuple() Constructor.py
+++ Python/Python Data types/Python Tuple/Create a Tuple Using tuple() Constructor.py
@@ -1,6 +1,4 @@
 # Create a Tuple Using tuple() Constructor
 
 tuple_constructor = tuple(('Jack', 'Maria', 'David'))
-print(tuple_constructor)
-
-# Output: ('Jack', 'Maria', 'David')
+print(tuple_constructor)      # Output: ('Jack', 'Maria', 'David')
