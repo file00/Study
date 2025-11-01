ae6cfbe23af2a87ed69443ea0d9de504cbe22eec
file00 <51076009+file00@users.noreply.github.com>
2025-11-02 08:06:56 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java char data type 2.java
@@ -0,0 +1,14 @@
+// Java char data type 2
+
+class Main {
+  public static void main(String[] args) {
+    	
+    char letter1 = '9';
+    System.out.println(letter1);  // prints 9
+    	
+    char letter2 = 65;
+    System.out.println(letter2);  // prints A
+
+  }
+}
+
