db655090c175d91611c406dd6485e8b2fc3abfce
file00 <51076009+file00@users.noreply.github.com>
2025-11-01 10:06:23 +0900

    Add files via upload

---
+++ Python/Python Files/Writing CSV files in Python/CSV files with custom quoting character/Writing CSV files with custom quoting character.py
@@ -0,0 +1,13 @@
+# Writing CSV files with custom quoting character
+
+import csv
+row_list = [
+    ["SN", "Name", "Quotes"],
+    [1, "Buddha", "What we think we become"],
+    [2, "Mark Twain", "Never regret anything that made you smile"],
+    [3, "Oscar Wilde", "Be yourself everyone else is already taken"]
+]
+with open('quotes.csv', 'w', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC,
+                        delimiter=';', quotechar='*')
+    writer.writerows(row_list)
