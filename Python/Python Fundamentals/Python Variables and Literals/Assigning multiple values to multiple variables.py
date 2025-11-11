5dc27db312ccacc9984dfb23f693b10d19fd4d18
file00 <51076009+file00@users.noreply.github.com>
2025-11-11 23:25:22 +0900

    Update Assigning multiple values to multiple variables.py

--- Python/Python Fundamentals/Python Variables and Literals/Assigning multiple values to multiple variables.py
+++ Python/Python Fundamentals/Python Variables and Literals/Assigning multiple values to multiple variables.py
@@ -3,9 +3,9 @@
 a, b, c = 5, 3.2, 'Hello'
 x = y  = 'github.com'
 
-print (a)      # prints 5
-print (b)      # prints 3.2
-print (c)      # prints Hello 
+print (a)      
+print (b)      
+print (c)     
 
-print (x)      # prints github.com
-print (y)      # prints github.com
+print (x)      
+print (y)      
