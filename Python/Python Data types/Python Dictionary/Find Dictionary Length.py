e9a58e219c1345bc540a5540fa18e314a8da8bde
file00 <51076009+file00@users.noreply.github.com>
2025-11-14 22:24:54 +0900

    Update Find Dictionary Length.py

--- Python/Python Data types/Python Dictionary/Find Dictionary Length.py
+++ Python/Python Data types/Python Dictionary/Find Dictionary Length.py
@@ -3,15 +3,16 @@
 country_capitals = {"England": "London", "Italy": "Rome"}
 
 # get dictionary's length
-print(len(country_capitals))      # Output: 2
+print(len(country_capitals))     
 
 numbers = {10: "ten", 20: "twenty", 30: "thirty"}
 
 # get dictionary's length
-print(len(numbers))      # Output: 3
+print(len(numbers))    
 
 countries = {}
 
 # get dictionary's length
 
-print(len(countries))      # Output: 0
+print(len(countries))     
+
