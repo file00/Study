df363f19c3a83cbcaaaf5af340084fa78cfb2f65
file00 <51076009+file00@users.noreply.github.com>
2025-11-17 12:21:17 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level2/Snowman (Amateur).c
@@ -0,0 +1,13 @@
+// Snowman (ASCII Art)
+
+#include <stdio.h>
+
+int main() {
+    printf("        ___\n");
+    printf("      _[___]_   \n");
+    printf("       (o o)    \n");
+    printf("   --- ( : ) ---\n");
+    printf("       ( : )   \n");
+    printf("      (_____)  \n");
+    return 0;
+}
