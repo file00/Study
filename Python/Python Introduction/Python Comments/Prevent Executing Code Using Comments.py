4ea87a1c799443b1b7ea4d5f1e44256dac059c33
file00 <51076009+file00@users.noreply.github.com>
2025-11-20 11:32:25 +0900

    Update Prevent Executing Code Using Comments.py

--- Python/Python Introduction/Python Comments/Prevent Executing Code Using Comments.py
+++ Python/Python Introduction/Python Comments/Prevent Executing Code Using Comments.py
@@ -6,7 +6,7 @@ number2 = 15
 sum = number1 + number2
 
 print("The sum is:\n", sum)
-print('The product is:\n', product)      # Error!!
+print('The product is:\n', product)   # Error!!
 
 print("The sum is:", sum)
 # print('The product is:', product)
