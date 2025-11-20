a2f046c790db1dd80b2111cb7771aaf35854a36f
file00 <51076009+file00@users.noreply.github.com>
2025-11-21 08:31:51 +0900

    Add files via upload

---
+++ Python/Python Data types/Python List/List Items of Different Types.py
@@ -0,0 +1,9 @@
+# List Items of Different Types
+
+# a list containing strings, numbers and another list
+student = ['Jack', 32, 'Computer Science', [2, 4]]
+print(student)
+
+# an empty list
+empty_list = []
+print(empty_list)
