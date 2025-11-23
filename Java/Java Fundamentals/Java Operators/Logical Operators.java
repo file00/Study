74bced5a0f0f9f5186e95a60cd164c47b77ad84b
file00 <51076009+file00@users.noreply.github.com>
2025-11-23 10:18:23 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Operators/Logical Operators.java
@@ -0,0 +1,19 @@
+// Logical Operators
+
+class Main {
+  public static void main(String[] args) {
+
+    // && operator
+    System.out.println((5 > 3) && (8 > 5));  
+    System.out.println((5 > 3) && (8 < 5));  
+
+    // || operator
+    System.out.println((5 < 3) || (8 > 5));  
+    System.out.println((5 > 3) || (8 < 5));  
+    System.out.println((5 < 3) || (8 < 5));  
+
+    // ! operator
+    System.out.println(!(5 == 3));  
+    System.out.println(!(5 > 3));  
+  }
+}
