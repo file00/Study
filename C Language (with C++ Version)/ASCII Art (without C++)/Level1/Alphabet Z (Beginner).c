338dd122b5624cc8d7ff36e6c7ad87a496daf189
file00 <51076009+file00@users.noreply.github.com>
2025-11-28 11:57:04 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Alphabet Z (Beginner).c
@@ -0,0 +1,13 @@
+// Alphabet Z (ASCII Art)
+#include <stdio.h>
+
+int main() {
+    printf("   ZZZZZZZ   \n");
+    printf("         Z   \n");
+    printf("        Z    \n");
+    printf("      Z      \n");
+    printf("     Z       \n");
+    printf("   Z         \n");
+    printf("   ZZZZZZZ   \n");
+    return 0;
+}
