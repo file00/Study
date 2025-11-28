1d60feadb8d76d127b62ef585cdbf13d60f261fc
file00 <51076009+file00@users.noreply.github.com>
2025-11-28 09:41:53 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Number 0 (Beginner).c
@@ -0,0 +1,13 @@
+// Number 0 (ASCII Art)
+#include <stdio.h>
+
+int main() {
+    printf("    00000    \n");
+    printf("   0     0   \n");
+    printf("   0     0   \n");
+    printf("   0     0   \n");
+    printf("   0     0   \n");
+    printf("   0     0   \n");
+    printf("    00000    \n");
+    return 0;
+}
