01324294a2d5782e66be348e00ebb213a43525ea
file00 <51076009+file00@users.noreply.github.com>
2025-11-30 08:59:12 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Operators/Java Ternary Operator.java
@@ -0,0 +1,13 @@
+// Java Ternary Operator
+
+class Java {
+  public static void main(String[] args) {
+
+    int februaryDays = 29;
+    String result;
+
+    // ternary operator
+    result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+    System.out.println(result);
+  }
+}
---
+++ Java/Java Fundamentals/Java Operators/Java instanceof Operator.java
@@ -0,0 +1,14 @@
+// Java instanceof Operator
+
+class Main {
+  public static void main(String[] args) {
+
+    String str = "Programiz";
+    boolean result;
+
+    // checks if str is an instance of
+    // the String class
+    result = str instanceof String;
+    System.out.println("Is str an object of String? " + result);
+  }
+}
