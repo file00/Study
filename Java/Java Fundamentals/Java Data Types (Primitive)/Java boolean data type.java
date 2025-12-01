6f172b996397d79ecf8848b3a9ec5ea6274a2dee
file00 <51076009+file00@users.noreply.github.com>
2025-12-01 11:54:39 +0900

    Update Java boolean data type.java

--- Java/Java Fundamentals/Java Data Types (Primitive)/Java boolean data type.java
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java boolean data type.java
@@ -4,6 +4,6 @@ class Main {
   public static void main(String[] args) {
     	
     boolean flag = true;
-    System.out.println(flag);    // prints true
+    System.out.println(flag);   // prints true
   }
 }
