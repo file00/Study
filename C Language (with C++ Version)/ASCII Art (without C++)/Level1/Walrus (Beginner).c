98fd71b011f6bdabf156547f997c998cbc86298a
file00 <51076009+file00@users.noreply.github.com>
2025-12-02 19:20:12 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Walrus (Beginner).c
@@ -0,0 +1,18 @@
+// Walrus (ASCII Art)
+#include <stdio.h>
+
+int main(void) {
+    printf("        .--.\n");
+    printf("       |o_o |\n");
+    printf("       |:_/ |\n");
+    printf("      //   \\ \\\n");
+    printf("     (|     | )\n");
+    printf("    /'\\_   _/`\\\n");
+    printf("    \\___)=(___/\n");
+    printf("     /  /| |\\  \\\n");
+    printf("    (  ( | | )  )\n");
+    printf("     \\  \\| |/  /\n");
+    printf("      '--' '--'\n");
+    printf("      WALRUS\n");
+    return 0;
+}
