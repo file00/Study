b6648b3045067cd18265b8815133ff980ab2bac9
file00 <51076009+file00@users.noreply.github.com>
2025-12-03 11:17:40 +0900

    Update Constants.cpp

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Constants.cpp
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Constants.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main() {
     const double PI = 3.14;
 
-    cout << fixed << setprecision(6) << PI << endl; // 3.140000
-    cout << fixed << setprecision(2) << PI;         // 3.14
+    cout << fixed << setprecision(6) << PI << endl;   // 3.140000
+    cout << fixed << setprecision(2) << PI;   // 3.14
 
     return 0;
-
 }
+
