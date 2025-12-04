547c2c989a5cf37e102d42970e3436f1e2089742
file00 <51076009+file00@users.noreply.github.com>
2025-12-05 00:14:59 +0900

    Update Integer Input & Output.cpp

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Input & Output.cpp
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Input & Output.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main() {
+
     int testInteger;
+    
     cout << "Enter an integer: ";
     cin >> testInteger;
     cout << "Number = " << testInteger;
+    
     return 0;
 }
