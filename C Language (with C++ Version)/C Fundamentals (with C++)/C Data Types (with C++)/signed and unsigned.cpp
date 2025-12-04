32bfd40219fcc7885025d6f403e50a89f8706915
file00 <51076009+file00@users.noreply.github.com>
2025-12-04 09:21:15 +0900

    Update signed and unsigned.cpp

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Data Types (with C++)/signed and unsigned.cpp
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Data Types (with C++)/signed and unsigned.cpp
@@ -1,13 +1,13 @@
 // signed and unsigned
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    
     // valid codes
     unsigned int x = 35;
     int y = -35;   // signed int
-    int z = 36;    // signed int
+    int z = 36;   // signed int
 
     cout << "x (unsigned int) = " << x << endl;
     cout << "y (signed int) = " << y << endl;
