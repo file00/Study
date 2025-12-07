864a74ed39aae234d9c95cb5c458c5b31ec5745f
file00 <51076009+file00@users.noreply.github.com>
2025-12-07 22:08:05 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Triangle 1 (Beginner).c
@@ -0,0 +1,12 @@
+// Triangle 1 (ASCII Art) 
+
+#include <stdio.h>
+
+int main() {
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf(" *     * \n");
+    printf("*********\n");
+    return 0;
+}
