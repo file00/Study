295379eb5cdfaa8bbfac525f98ca7fc738b76241
file00 <51076009+file00@users.noreply.github.com>
2025-12-08 12:08:02 +0900

    Update Assignment Operators.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Assignment Operators.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Assignment Operators.c
@@ -1,21 +1,21 @@
 // Assignment Operators 
 // Working of assignment operators
 #include <stdio.h>
-int main()
-{
+int main() {
+    
     int a = 5, c;
 
-    c = a;      // c is 5
+    c = a;   // c is 5
     printf("c = %d\n", c);
-    c += a;     // c is 10 
+    c += a;   // c is 10 
     printf("c = %d\n", c);
-    c -= a;     // c is 5
+    c -= a;   // c is 5
     printf("c = %d\n", c);
-    c *= a;     // c is 25
+    c *= a;   // c is 25
     printf("c = %d\n", c);
-    c /= a;     // c is 5
+    c /= a;   // c is 5
     printf("c = %d\n", c);
-    c %= a;     // c = 0
+    c %= a;   // c = 0
     printf("c = %d\n", c);
 
     return 0;
