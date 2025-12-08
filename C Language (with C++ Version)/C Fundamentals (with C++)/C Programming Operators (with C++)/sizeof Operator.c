33e7f0cae2f1544788aa92301ac53ae7ae6ca097
file00 <51076009+file00@users.noreply.github.com>
2025-12-08 12:09:44 +0900

    Update sizeof Operator.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/sizeof Operator.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/sizeof Operator.c
@@ -1,11 +1,13 @@
 // sizeof Operator 
 #include <stdio.h>
+
 int main()
 {
     int a;
     float b;
     double c;
     char d;
+    
     printf("Size of int=%lu bytes\n",sizeof(a));
     printf("Size of float=%lu bytes\n",sizeof(b));
     printf("Size of double=%lu bytes\n",sizeof(c));
