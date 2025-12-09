5354673b1c865ded58faa04dd8d9cb61a710d4a8
file00 <51076009+file00@users.noreply.github.com>
2025-12-09 10:50:38 +0900

    Update Single-line Comments in C.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Single-line Comments in C.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Single-line Comments in C.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main() {
+  
   // create integer variable    
   int age = 25; 
  
-  // print the age variable
-  printf("Age: %d", age);
+  printf("Age: %d", age);   // print the age variable
 
   return 0;
 }
