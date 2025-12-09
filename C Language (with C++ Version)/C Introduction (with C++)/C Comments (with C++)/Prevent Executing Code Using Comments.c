e582f06bdbd331d6710e8004545671f0929fd7a1
file00 <51076009+file00@users.noreply.github.com>
2025-12-09 10:51:41 +0900

    Update Prevent Executing Code Using Comments.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 
 int main() {
+    
     int number1 = 10;
     int number2 = 15;
+    
     int sum = number1 + number2;
 
     printf("The sum is: %d\n", sum);
     // printf("The product is: %d\n", product); 
+    
     return 0;
 }
