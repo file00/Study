17d825fb9aaf087c8a30e7c8c7fb7560d5234c71
file00 <51076009+file00@users.noreply.github.com>
2025-12-22 11:11:58 +0900

    Update Float and Double Input & Output.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Float and Double Input & Output.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Float and Double Input & Output.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 int main()
 {
+    
     float num1;
     double num2;
 
     printf("Enter a number: ");
     scanf("%f", &num1);
+    
     printf("Enter another number: ");
     scanf("%lf", &num2);
 
