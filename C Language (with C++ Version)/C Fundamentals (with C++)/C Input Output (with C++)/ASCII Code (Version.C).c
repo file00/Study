5240e8c59a4a9f1f81ea4e893430992dd8bd2660
file00 <51076009+file00@users.noreply.github.com>
2025-12-24 10:36:02 +0900

    Update ASCII Code (Version.C).c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Code (Version.C).c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Code (Version.C).c
@@ -1,16 +1,25 @@
-// ASCII Code (Version.C) 
+// ASCII Code (Version.C)
+// This program reads a character and prints its ASCII value
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    
-    char chr;
-    
-    printf("Enter a character: ");
-    scanf("%c", &chr);
 
+    char chr;
+
+    // Prompt the user to enter a character
+    printf("Enter a character: ");
+
+    // Read a single character
+    // Leading space ignores whitespace (e.g., newline)
+    scanf(" %c", &chr);
+
+    // Output the entered character
     printf("You entered %c.\n", chr);
+
+    // Output the ASCII value of the character
     printf("ASCII value is %d.", chr);
-    
+
     return 0;
 }
