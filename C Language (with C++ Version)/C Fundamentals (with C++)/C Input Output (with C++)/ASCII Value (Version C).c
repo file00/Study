dca389d7de62f47ba77ddf9b83e9b73538763602
file00 <51076009+file00@users.noreply.github.com>
2025-12-24 10:36:20 +0900

    Update ASCII Value (Version C).c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Value (Version C).c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/ASCII Value (Version C).c
@@ -1,18 +1,25 @@
-// ASCII Value 
+// ASCII Value
+// This program demonstrates how characters are stored as ASCII values
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    
+
     char chr;
-    
+
+    // Ask the user for a character input
     printf("Enter a character: ");
-    scanf("%c", &chr);     
 
-    // When %c is used, a character is displayed
-    printf("You entered %c.\n",chr);  
+    // Read one character from input
+    // The space before %c prevents reading leftover newline characters
+    scanf(" %c", &chr);
 
-    // When %d is used, ASCII value is displayed
-    printf("ASCII value is %d.", chr);  
-    
+    // When %c is used, the character itself is displayed
+    printf("You entered %c.\n", chr);
+
+    // When %d is used, the ASCII (integer) value of the character is displayed
+    printf("ASCII value is %d.", chr);
+
     return 0;
 }
