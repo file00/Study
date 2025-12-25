a7f10ca12823a3872e2ab57a6e9dfc1284d9b761
file00 <51076009+file00@users.noreply.github.com>
2025-12-26 01:06:40 +0900

    Update Float and Double Input & Output.cpp

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Float and Double Input & Output.cpp
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Float and Double Input & Output.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int main() {
+    
     float num1;
     double num2;
 
     cout << "Enter a number: ";
     cin >> num1;
+    
     cout << "Enter another number: ";
     cin >> num2;
 
