fe3bf36ce4c29cd4e11f28957f56028d348fc621
file00 <51076009+file00@users.noreply.github.com>
2025-12-25 10:54:55 +0900

    Update Integer Input & Output.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Input & Output.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Integer Input & Output.c
@@ -1,14 +1,15 @@
 // Integer Input/Output 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main()
 {
     int testInteger;
     
     printf("Enter an integer: ");
     scanf("%d", &testInteger);  
+    
     printf("Number = %d",testInteger);
     
     return 0;
 }
-
-
