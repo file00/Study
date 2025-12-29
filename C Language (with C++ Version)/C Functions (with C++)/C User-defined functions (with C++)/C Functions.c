0dc67f8929e4c3f719edce33aa7b5c7db1a133bb
file00 <51076009+file00@users.noreply.github.com>
2025-12-30 07:45:18 +0900

    Update C Functions.c

--- C Language (with C++ Version)/C Functions (with C++)/C User-defined functions (with C++)/C Functions.c
+++ C Language (with C++ Version)/C Functions (with C++)/C User-defined functions (with C++)/C Functions.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 
 int Sum(int value1, int value2) {
+    
     int result = value1 + value2;
+    
     return result;
 }
 
 int main() {
+    
     int a = 2, b = 3, value;
     value = Sum(a, b);
     
