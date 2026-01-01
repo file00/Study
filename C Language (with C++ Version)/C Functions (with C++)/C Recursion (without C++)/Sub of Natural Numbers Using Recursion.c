cc9f4288a7fd6e2020bfe1a6270ee7905f81625d
file00 <51076009+file00@users.noreply.github.com>
2026-01-02 07:13:15 +0900

    Update Sub of Natural Numbers Using Recursion.c

--- C Language (with C++ Version)/C Functions (with C++)/C Recursion (without C++)/Sub of Natural Numbers Using Recursion.c
+++ C Language (with C++ Version)/C Functions (with C++)/C Recursion (without C++)/Sub of Natural Numbers Using Recursion.c
@@ -12,11 +12,13 @@ int main() {
     result = sum(number);
 
     printf("sum = %d", result);
+    
     return 0;
 }
 
 int sum(int n) {
     if (n != 0)
+        
         // sub() function calls itself
         return n - sum(n-1); 
     else
