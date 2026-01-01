44c35b252b8f2a51a3fcdc4ddb239ebc4ab535bb
file00 <51076009+file00@users.noreply.github.com>
2026-01-01 09:49:14 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Input & Output (Basic Version C).c
@@ -0,0 +1,14 @@
+// Input & Output (Basic Version)
+#include <stdio.h>
+
+int main()
+{
+    int sum;
+    
+    printf("Input : ");
+    scanf("%d", &sum);
+    
+    printf("Output : %d\n", sum);
+
+    return 0;
+}
