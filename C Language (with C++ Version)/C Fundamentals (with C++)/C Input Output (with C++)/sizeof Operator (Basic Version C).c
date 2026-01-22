ff84ec09241ba0bd9dce9779f7e7e13171e0d7b9
file00 <51076009+file00@users.noreply.github.com>
2026-01-22 22:02:20 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/sizeof Operator (Basic Version C).c
@@ -0,0 +1,30 @@
+// sizeof Operator (Basic Version C)
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+
+    printf("Enter int value: ");
+    scanf("%d", &a);
+
+    printf("Enter float value: ");
+    scanf("%f", &b);
+
+    printf("Enter double value: ");
+    scanf("%lf", &c);
+
+    printf("Enter char value: ");
+    scanf(" %c", &d);   
+
+    printf("\n--- Sizeof Results ---\n");
+    printf("Size of int    = %zu bytes\n", sizeof(a));
+    printf("Size of float  = %zu bytes\n", sizeof(b));
+    printf("Size of double = %zu bytes\n", sizeof(c));
+    printf("Size of char   = %zu byte\n", sizeof(d));
+
+    return 0;
+}
