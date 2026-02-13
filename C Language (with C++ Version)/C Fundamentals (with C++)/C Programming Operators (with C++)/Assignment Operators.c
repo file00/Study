eec075137382cbc953105c49d4e0a796ff4a5c1e
file00 <51076009+file00@users.noreply.github.com>
2026-02-14 08:56:01 +0900

    Update Assignment Operators.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Assignment Operators.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Assignment Operators.c
@@ -8,16 +8,22 @@ int main() {
 
     c = a;   // c is 5
     printf("c = %d\n", c);
+    
     c += a;   // c is 10 
     printf("c = %d\n", c);
+    
     c -= a;   // c is 5
     printf("c = %d\n", c);
+    
     c *= a;   // c is 25
     printf("c = %d\n", c);
+    
     c /= a;   // c is 5
     printf("c = %d\n", c);
+    
     c %= a;   // c = 0
     printf("c = %d\n", c);
 
     return 0;
 }
+
