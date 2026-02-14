caa8be7cb4dd9e783f57d4fe742fa19964be7238
file00 <51076009+file00@users.noreply.github.com>
2026-02-15 08:19:03 +0900

    Update Arithmetic Operators.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Arithmetic Operators.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Arithmetic Operators.c
@@ -8,15 +8,18 @@ int main() {
     
     c = a+b;
     printf("a+b = %d \n",c);
+    
     c = a-b;
     printf("a-b = %d \n",c);
+    
     c = a*b;
     printf("a*b = %d \n",c);
+    
     c = a/b;
     printf("a/b = %d \n",c);
+    
     c = a%b;
     printf("Remainder when a divided by b = %d \n",c);
     
     return 0;
 }
-
