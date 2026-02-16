92863d0ca55c116d58085b897b9f52c2eda3818d
file00 <51076009+file00@users.noreply.github.com>
2026-02-17 00:54:28 +0900

    Update for loop (Input & Output).c

--- C Language (with C++ Version)/C Flow Control (with C++)/C for Loop (with C++)/for loop (Input & Output).c
+++ C Language (with C++ Version)/C Flow Control (with C++)/C for Loop (with C++)/for loop (Input & Output).c
@@ -12,8 +12,7 @@ int main() {
     scanf("%d", &num);
 
     // for loop terminates when num is less than count
-    for(count = 1; count <= num; ++count)
-    {
+    for(count = 1; count <= num; ++count) {
         sum += count;
     }
 
