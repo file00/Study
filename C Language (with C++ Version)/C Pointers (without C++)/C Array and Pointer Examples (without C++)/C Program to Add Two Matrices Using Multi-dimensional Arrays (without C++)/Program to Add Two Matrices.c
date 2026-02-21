ecbd5046a192af1399c235c5d2abccd0cc51ee1a
file00 <51076009+file00@users.noreply.github.com>
2026-02-22 06:07:47 +0900

    Update Program to Add Two Matrices.c

--- C Language (with C++ Version)/C Pointers (without C++)/C Array and Pointer Examples (without C++)/C Program to Add Two Matrices Using Multi-dimensional Arrays (without C++)/Program to Add Two Matrices.c
+++ C Language (with C++ Version)/C Pointers (without C++)/C Array and Pointer Examples (without C++)/C Program to Add Two Matrices Using Multi-dimensional Arrays (without C++)/Program to Add Two Matrices.c
@@ -1,15 +1,18 @@
 // Program to Add Two Matrices
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main() {
+  
   int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+  
   printf("Enter the number of rows (between 1 and 100): ");
   scanf("%d", &r);
   printf("Enter the number of columns (between 1 and 100): ");
   scanf("%d", &c);
 
   printf("\nEnter elements of 1st matrix:\n");
+  
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
       printf("Enter element a%d%d: ", i + 1, j + 1);
