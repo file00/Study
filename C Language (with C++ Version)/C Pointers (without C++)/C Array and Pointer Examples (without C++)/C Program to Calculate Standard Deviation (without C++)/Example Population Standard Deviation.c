ed1bb4d2fe20196a46dc782084499f98abe644f0
file00 <51076009+file00@users.noreply.github.com>
2026-02-23 00:08:38 +0900

    Update Example Population Standard Deviation.c

--- C Language (with C++ Version)/C Pointers (without C++)/C Array and Pointer Examples (without C++)/C Program to Calculate Standard Deviation (without C++)/Example Population Standard Deviation.c
+++ C Language (with C++ Version)/C Pointers (without C++)/C Array and Pointer Examples (without C++)/C Program to Calculate Standard Deviation (without C++)/Example Population Standard Deviation.c
@@ -5,14 +5,19 @@
 // SD of a population
 #include <math.h>
 #include <stdio.h>
+
 float calculateSD(float data[]);
+
 int main() {
+    
     int i;
     float data[10];
     printf("Enter 10 elements: ");
+    
     for (i = 0; i < 10; ++i)
         scanf("%f", &data[i]);
     printf("\nStandard Deviation = %.6f", calculateSD(data));
+    
     return 0;
 }
 
@@ -26,5 +31,6 @@ float calculateSD(float data[]) {
     for (i = 0; i < 10; ++i) {
         SD += pow(data[i] - mean, 2);
     }
+    
     return sqrt(SD / 10);
 }
