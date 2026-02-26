ac3b490809cb7041e019ec89d7bc74fbf423d29a
file00 <51076009+file00@users.noreply.github.com>
2026-02-27 05:54:45 +0900

    Update calloc() and free().c

--- C Language (with C++ Version)/C Pointers (without C++)/C Dynamic Memory Allocation (without C++)/calloc() and free().c
+++ C Language (with C++ Version)/C Pointers (without C++)/C Dynamic Memory Allocation (without C++)/calloc() and free().c
@@ -1,15 +1,18 @@
 // calloc() and free() 
 // Program to calculate the sum of n numbers entered by the user
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
+  
   int n, i, *ptr, sum = 0;
+  
   printf("Enter number of elements: ");
   scanf("%d", &n);
 
   ptr = (int*) calloc(n, sizeof(int));
+  
   if(ptr == NULL) {
     printf("Error! memory not allocated.");
     exit(0);
@@ -23,5 +26,6 @@ int main() {
 
   printf("Sum = %d", sum);
   free(ptr);
+  
   return 0;
 }
