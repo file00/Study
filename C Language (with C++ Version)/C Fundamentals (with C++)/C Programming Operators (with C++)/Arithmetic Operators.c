0a2f789c7c1852df8d7be569461d70cebfa4edb5
file00 <51076009+file00@users.noreply.github.com>
2026-03-14 00:19:38 +0900

    Update Arithmetic Operators.c

--- C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Arithmetic Operators.c
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Programming Operators (with C++)/Arithmetic Operators.c
@@ -1,24 +1,25 @@
 // Arithmetic Operators 
 // Working of arithmetic operators
+
 #include <stdio.h>
 
 int main() {
     
     int a = 9, b = 4, c;
     
-    c = a+b;
+    c = a + b;
     printf("a+b = %d \n",c);
     
-    c = a-b;
+    c = a - b;
     printf("a-b = %d \n",c);
     
-    c = a*b;
+    c = a * b;
     printf("a*b = %d \n",c);
     
-    c = a/b;
+    c = a / b;
     printf("a/b = %d \n",c);
     
-    c = a%b;
+    c = a % b;
     printf("Remainder when a divided by b = %d \n",c);
     
     return 0;
