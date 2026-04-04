a7c13dbcd782935eee40ff0955bd221bf1be3f3b
file00 <51076009+file00@users.noreply.github.com>
2026-04-05 00:18:49 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C++ Program (with Escape Sequence).cpp
@@ -0,0 +1,12 @@
+// My First C++ Program (with Escape Sequence)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World" << endl;
+    cout << "I love C++!!";
+    
+    return 0;
+}
