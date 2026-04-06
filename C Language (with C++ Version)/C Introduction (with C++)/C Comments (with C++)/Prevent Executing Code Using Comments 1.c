13b46e67da43cea56aa61a9a975e02d25f1c5204
file00 <51076009+file00@users.noreply.github.com>
2026-04-06 09:26:50 +0900

    Update Prevent Executing Code Using Comments 1.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 1.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 1.c
@@ -8,7 +8,14 @@ int main() {
     int number2 = 15;
     int sum = number1 + number2;
 
+    // Print the sum (This code will execute)
     printf("The sum is: %d\n", sum);
+
+    /* 
+       The code below is commented out and will not be executed.
+       Uncommenting it without defining 'product' would cause a compiler error.
+    */
+    // int product = number1 * number2;
     // printf("The product is: %d\n", product); 
     
     return 0;
