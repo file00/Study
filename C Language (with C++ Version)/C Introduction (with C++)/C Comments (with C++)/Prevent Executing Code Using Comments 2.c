f24996f9559045328717b00517b6ae0bd1429902
file00 <51076009+file00@users.noreply.github.com>
2026-04-07 00:02:02 +0900

    Update Prevent Executing Code Using Comments 2.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 2.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 2.c
@@ -5,11 +5,17 @@
 int main() {
     
     int number1 = 10, number2 = 15;
-    int sum = number1 + number2;
 
-    printf("The sum is: %d\n", sum);
-    // printf("The product is: %d\n", product); 
+    /* 
+       The following 'sum' block is commented out 
+       to prevent its execution as per the file title.
+    */
+    // int sum = number1 + number2;
+    // printf("The sum is: %d\n", sum);
+
+    // This section is active and will execute.
+    int product = number1 * number2;
+    printf("The product is: %d\n", product); 
     
     return 0;
 }
-
