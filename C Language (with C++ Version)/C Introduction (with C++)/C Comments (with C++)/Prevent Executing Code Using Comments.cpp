9fe58c5cb26803624d825609a16f2f44430f43bd
file00 <51076009+file00@users.noreply.github.com>
2026-04-08 06:42:46 +0900

    Update Prevent Executing Code Using Comments.cpp

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments.cpp
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main() {
     
+    /* The following code is prevented from executing using comments:
     int number1 = 10;
     int number2 = 15;
     int sum = number1 + number2;
  
-    cout << "The sum is: " << sum;   // print the age variable
+    cout << "The sum is: " << sum; 
+    */
 
+    // return 0; // Even the return statement can be commented out
     return 0;
 }
