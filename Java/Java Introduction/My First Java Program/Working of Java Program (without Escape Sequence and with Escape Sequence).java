8ae5e23267538dc6c9e0c934d5d9221f3c32c9d0
file00 <51076009+file00@users.noreply.github.com>
2026-04-10 07:58:36 +0900

    Add files via upload

---
+++ Java/Java Introduction/My First Java Program/Working of Java Program (without Escape Sequence and with Escape Sequence).java
@@ -0,0 +1,10 @@
+// Working of Java Program (without Escape Sequence and with Escape Sequence)
+
+public class Main { 
+
+    public static void main(String[] args) {
+        
+        System.out.println("Java is fun!");
+        System.out.print("Hello, World!");
+    }
+}
---
+++ Java/Java Introduction/My First Java Program/Working of Java Program (without Escape Sequence).java
@@ -0,0 +1,9 @@
+// Working of Java Program (without Escape Sequence)
+
+public class Main { 
+
+    public static void main(String[] args) {
+        
+        System.out.print("Hello, World!");
+    }
+}
