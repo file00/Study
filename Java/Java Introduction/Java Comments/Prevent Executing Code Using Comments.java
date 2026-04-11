5d3cb5183d0aaac1434607fb00b06cf1428cf027
file00 <51076009+file00@users.noreply.github.com>
2026-04-12 06:45:16 +0900

    Update Prevent Executing Code Using Comments.java

--- Java/Java Introduction/Java Comments/Prevent Executing Code Using Comments.java
+++ Java/Java Introduction/Java Comments/Prevent Executing Code Using Comments.java
@@ -1,9 +1,20 @@
 // Prevent Executing Code Using Comments
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         System.out.println("some code");
-        System.out.println("error code");
+
+        // Prevent the execution of the following line
+        // System.out.println("error code"); 
+
+        /* 
+           The following code is also disabled 
+           using a multi-line comment 
+        */
+        // System.out.println("another error code");
+
         System.out.println("some other code");
     }
 }
