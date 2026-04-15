2234f6df647755e95ca927fe02718e8204156e9a
file00 <51076009+file00@users.noreply.github.com>
2026-04-16 08:15:05 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/String Literals.c
@@ -0,0 +1,22 @@
+// String Literals
+
+#include <stdio.h>
+
+int main() {
+    // Display "good"
+    printf("\"good\"                  //string constant\n");
+
+    // Display ""
+    printf("\"\"                     //null string constant\n");
+
+    // Display "      "
+    printf("\"      \"               //string constant of six white space\n");
+
+    // Display "x"
+    printf("\"x\"                    //string constant having a single character.\n");
+
+    // Display "Earth is round\n"
+    printf("\"Earth is round\\n\"         //prints string with a newline\n");
+
+    return 0;
+}
