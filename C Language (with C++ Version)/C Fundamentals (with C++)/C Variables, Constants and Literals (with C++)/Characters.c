04d6f93ca038947a6cc293be31f17069bedb6acc
file00 <51076009+file00@users.noreply.github.com>
2026-04-16 08:14:51 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Variables, Constants and Literals (with C++)/Characters.c
@@ -0,0 +1,19 @@
+// Characters
+
+#include <stdio.h>
+
+int main() {
+    
+    // Define character constants
+    char a = 'a';
+    char m = 'm';
+    char f = 'F';
+    char two = '2';
+    char brace = '}';
+
+    // Print the descriptive text using character literals
+    printf("A character literal is created by enclosing a single character inside single quotation marks. \n");
+    printf("For example: '%c', '%c', '%c', '%c', '%c' etc.\n", a, m, f, two, brace);
+
+    return 0;
+}
