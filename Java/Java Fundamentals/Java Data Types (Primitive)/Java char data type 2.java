4f618ac7e000c37b3a10262e739d1088778c760e
file00 <51076009+file00@users.noreply.github.com>
2026-04-22 10:17:52 +0900

    Update Java char data type 2.java

--- Java/Java Fundamentals/Java Data Types (Primitive)/Java char data type 2.java
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java char data type 2.java
@@ -1,6 +1,7 @@
 // Java char data type 2
 
 class Main {
+  
   public static void main(String[] args) {
     	
     char letter1 = '9';
@@ -8,7 +9,6 @@ class Main {
     	
     char letter2 = 65;
     System.out.println(letter2);  // prints A
-
   }
 }
 
