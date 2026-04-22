e020f481a86164a4244586ea4dfddf0bdce99251
file00 <51076009+file00@users.noreply.github.com>
2026-04-22 10:17:34 +0900

    Update Java char data type 1.java

--- Java/Java Fundamentals/Java Data Types (Primitive)/Java char data type 1.java
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java char data type 1.java
@@ -1,9 +1,11 @@
 // Java char data type 1
 
 class Main {
+  
   public static void main(String[] args) {
     	
     char letter = '\u0051';
+    
     System.out.println(letter);  // prints Q
   }
 }
