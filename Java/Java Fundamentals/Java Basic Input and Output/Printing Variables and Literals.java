0205695f370939faad0ce5ea649dad8c59ef1958
file00 <51076009+file00@users.noreply.github.com>
2026-04-25 08:17:37 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Basic Input and Output/Printing Variables and Literals.java
@@ -0,0 +1,12 @@
+// Printing Variables and Literals
+
+class Variables {
+    
+    public static void main(String[] args) {
+    	
+        Double number = -10.6;
+    	
+        System.out.println(5);
+        System.out.println(number);
+    }
+}
