adba3ef011ba621395a01e1001fe7f7797960d95
file00 <51076009+file00@users.noreply.github.com>
2026-04-26 07:55:41 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Basic Input and Output/Print Concatenated Strings.java
@@ -0,0 +1,11 @@
+// Print Concatenated Strings
+class PrintVariables {
+    
+    public static void main(String[] args) {
+    	
+        Double number = -10.6;
+    	
+        System.out.println("I am " + "awesome.");
+        System.out.println("Number = " + number);
+    }
+}
