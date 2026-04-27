1447472836860de7070ab5a6576aac1aaf18032a
file00 <51076009+file00@users.noreply.github.com>
2026-04-28 06:03:30 +0900

    Update Java Ternary Operator.java

--- Java/Java Fundamentals/Java Operators/Java Ternary Operator.java
+++ Java/Java Fundamentals/Java Operators/Java Ternary Operator.java
@@ -1,6 +1,7 @@
 // Java Ternary Operator
 
 class Java {
+  
   public static void main(String[] args) {
 
     int februaryDays = 29;
@@ -8,6 +9,7 @@ class Java {
 
     // ternary operator
     result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+    
     System.out.println(result);
   }
 }
