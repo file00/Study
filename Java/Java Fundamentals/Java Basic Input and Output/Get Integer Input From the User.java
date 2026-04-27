3fcabea17e6001310acee7ac371e8304d6107151
file00 <51076009+file00@users.noreply.github.com>
2026-04-28 06:05:56 +0900

    Add files via upload

---
+++ Java/Java Fundamentals/Java Basic Input and Output/Get Integer Input From the User.java
@@ -0,0 +1,19 @@
+// Get Integer Input From the User
+
+import java.util.Scanner;
+
+class Input {
+    
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+    	
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        
+        System.out.println("You entered " + number);
+
+        // closing the scanner object
+        input.close();
+    }
+}
