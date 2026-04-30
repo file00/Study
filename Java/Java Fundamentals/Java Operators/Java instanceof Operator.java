ad62e6628ca555e4d2ceb531bbc8d8608d74bcf5
file00 <51076009+file00@users.noreply.github.com>
2026-04-30 17:21:00 +0900

    Update Java instanceof Operator.java

--- Java/Java Fundamentals/Java Operators/Java instanceof Operator.java
+++ Java/Java Fundamentals/Java Operators/Java instanceof Operator.java
@@ -1,6 +1,7 @@
 // Java instanceof Operator
 
 class Main {
+  
   public static void main(String[] args) {
 
     String str = "Programiz";
@@ -9,6 +10,7 @@ class Main {
     // checks if str is an instance of
     // the String class
     result = str instanceof String;
+    
     System.out.println("Is str an object of String? " + result);
   }
 }
