0f52ee322b6572561b804bc13fb5a0b2debc4e04
file00 <51076009+file00@users.noreply.github.com>
2026-05-01 07:05:49 +0900

    Update String Literals.java

--- Java/Java Fundamentals/Java Variables and Literals/String Literals.java
+++ Java/Java Fundamentals/Java Variables and Literals/String Literals.java
@@ -5,9 +5,9 @@ class Main {
   public static void main(String[] args) {
     	
     String str1 = "Java Programming";
-    String str2 = "Programiz";
+    String str2 = "Google";
 
     System.out.println(str1);  // Java Programming
-    System.out.println(str2);    // Programiz
+    System.out.println(str2);    // Google
   }
 }
