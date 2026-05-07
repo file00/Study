b78f9e04909dc2b480cf483d218041d4c56bef5a
file00 <51076009+file00@users.noreply.github.com>
2026-05-08 07:10:22 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/C Fundamentals (with C++)/C Input Output (with C++)/Print Characters (Input & Output Version).c
@@ -0,0 +1,17 @@
+// Print Characters (Input & Output Version)
+
+#include <stdio.h>
+
+int main() {
+    
+    char chr;
+
+    // Taking a single character as input
+    printf("Enter a character: ");
+    scanf("%c", &chr);
+
+    // Printing the entered character
+    printf("character = %c\n", chr);
+
+    return 0;
+}
