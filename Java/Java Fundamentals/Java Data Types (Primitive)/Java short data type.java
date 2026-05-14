686f07295a0f548907fa3eaa60829cf3a8dd7443
file00 <51076009+file00@users.noreply.github.com>
2026-05-14 09:19:33 +0900

    Update Java short data type.java

--- Java/Java Fundamentals/Java Data Types (Primitive)/Java short data type.java
+++ Java/Java Fundamentals/Java Data Types (Primitive)/Java short data type.java
@@ -1,10 +1,13 @@
 // Java short data type
 
-class Main {
+public class Main {
+  
   public static void main(String[] args) {
     	
     short temperature;
+    
     temperature = -200;
+    
     System.out.println(temperature);  // prints -200
   }
 }
