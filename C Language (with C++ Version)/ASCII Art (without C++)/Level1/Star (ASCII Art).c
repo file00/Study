c2c25573e2cc8e9c95d08338ec3c5d3b505ed726
file00 <51076009+file00@users.noreply.github.com>
2026-05-18 09:24:14 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Star (ASCII Art).c
@@ -0,0 +1,14 @@
+// Star (ASCII Art)
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("     *     \n");
+    printf("   *   *   \n");
+    printf(" *       * \n");
+    printf("   *   *   \n");
+    printf("     *     \n");
+    
+    return 0;
+}
