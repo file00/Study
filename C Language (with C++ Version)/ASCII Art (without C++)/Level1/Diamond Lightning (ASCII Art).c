9194520ded2e9f500abf9b44768b5d2e4df02270
file00 <51076009+file00@users.noreply.github.com>
2026-05-23 09:55:56 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Diamond Lightning (ASCII Art).c
@@ -0,0 +1,20 @@
+// Diamond Lightning (ASCII Art)
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("         /\\ \n");
+    printf("        /  \\ \n");
+    printf("       /m--n\\ \n");
+    printf("      /      \\ \n");
+    printf("     / /\\  /\\ \\ \n");
+    printf("    < m  --  n > \n");
+    printf("     \\ \\/  \\/ / \n");
+    printf("      \\      / \n");
+    printf("       \\m--n/ \n");
+    printf("        \\  / \n");
+    printf("         \\/ \n");
+
+    return 0;
+}
