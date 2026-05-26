57b08d8d7225978eaa8e2e4cfcefc8d89f70255e
file00 <51076009+file00@users.noreply.github.com>
2026-05-27 08:13:16 +0900

    Update Working of Java Program.java

--- Java/Java Introduction/My First Java Program/Working of Java Program.java
+++ Java/Java Introduction/My First Java Program/Working of Java Program.java
@@ -7,3 +7,6 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+
+// public class: "This is a representative class that can be used anywhere in our program!"
+// Just class: "This is a helper class that works quietly only inside this folder (package)!"
