c6ac73b7af6a27e3209715642f6451f160903522
file00 <51076009+file00@users.noreply.github.com>
2026-05-28 12:36:45 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java for Loop/Display a Text Five Times.java
@@ -0,0 +1,15 @@
+// Display a Text Five Times
+// Program to print a text 5 times
+
+public class Main {
+    
+  public static void main(String[] args) {
+
+    int n = 5;
+    // for loop  
+    
+    for (int i = 1; i <= n; ++i) {
+      System.out.println("Java is fun");
+    }
+  }
+}
