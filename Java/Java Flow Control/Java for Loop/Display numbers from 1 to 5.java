32ebd21d26ae5511f6fd1c1629872d15166a9cc4
file00 <51076009+file00@users.noreply.github.com>
2026-05-30 12:06:28 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java for Loop/Display numbers from 1 to 5.java
@@ -0,0 +1,15 @@
+// Display numbers from 1 to 5
+// Program to print numbers from 1 to 5
+
+public class Main {
+    
+  public static void main(String[] args) {
+  
+    int n = 5;
+    // for loop  
+    
+    for (int i = 1; i <= n; ++i) {
+      System.out.println(i);
+    }
+  }
+}
