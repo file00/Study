b2d6f7d76fd51209510fa64bca7593137994cf70
file00 <51076009+file00@users.noreply.github.com>
2026-06-03 01:16:49 +0900

    Update Program to create a simple calculator (with % operator).c

--- C Language (with C++ Version)/C Flow Control (with C++)/C switch Statement (with C++)/Program to create a simple calculator (with % operator).c
+++ C Language (with C++ Version)/C Flow Control (with C++)/C switch Statement (with C++)/Program to create a simple calculator (with % operator).c
@@ -12,8 +12,8 @@ int main() {
     printf("Enter two operands: ");
     scanf("%lf %lf", &n1, &n2);
 
-    switch(operation)
-    {
+    switch(operation) {
+        
         case '+':
             printf("%.1lf + %.1lf = %.1lf", n1, n2, n1+n2);
             break;
