46c48106b49e8bd8d2317935432f9d5c8b19de63
file00 <51076009+file00@users.noreply.github.com>
2026-06-06 11:59:08 +0900

    Update My First C Program (with Escape Sequence).c

--- C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (with Escape Sequence).c
+++ C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (with Escape Sequence).c
@@ -4,8 +4,21 @@
 
 int main() {
     
+    // The '\n' moves the cursor to the next line after printing.
     printf("Hello, World!\n");   
+    
+    // This text will start on a brand new line.
     printf("I love C Language!!");
+
+    /*
+     * [Understanding \n (New Line Escape Sequence)]
+     * - C does not automatically wrap text to a new line between printf statements.
+     * - \n acts like pressing the 'Enter' key on your keyboard.
+     * 
+     * Comparison:
+     * - With \n    -> Moves the cursor down. The next text goes to the next line.
+     * - Without \n -> Keeps the cursor there. The next text sticks right next to it.
+     */
 
     return 0;
 }
