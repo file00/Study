e51154cec1846087e5695499f6d5b97edb02e034
file00 <51076009+file00@users.noreply.github.com>
2026-06-08 06:13:51 +0900

    Update Prevent Executing Code Using Comments 1.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 1.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Prevent Executing Code Using Comments 1.c
@@ -1,4 +1,4 @@
-// Prevent Executing Code Using Comments 1
+// [Title] Preventing Code Execution Using Comments and Preprocessors 1
 
 #include <stdio.h>
 
@@ -8,15 +8,34 @@ int main() {
     int number2 = 15;
     int sum = number1 + number2;
 
-    // Print the sum (This code will execute)
+    // SECTION 1: Single-line comment example (This code will execute)
     printf("The sum is: %d\n", sum);
 
     /* 
+       SECTION 2: Multi-line comment example
        The code below is commented out and will not be executed.
-       Uncommenting it without defining 'product' would cause a compiler error.
+       If you uncomment the printf line below, it will cause a compiler error
+       because 'product' is not defined in this scope.
     */
     // int product = number1 * number2;
     // printf("The product is: %d\n", product); 
+
+    /*
+       SECTION 3: Advanced code disabling using Preprocessor Directives
+       Using '#if 0' and '#endif' is a safer way to disable large blocks of code,
+       especially when the code already contains multi-line comments inside it.
+    */
+    
+#if 0
+    int difference = number2 - number1;
+    printf("The difference is: %d\n", difference);
+    
+    /* 
+       Nested multi-line comments like this can cause syntax errors 
+       if you try to disable them using standard block comments (/* ... */).
+       That is why '#if 0' is preferred for debugging.
+    */
+#endif
     
     return 0;
 }
