23f689c43522cca13e84845551c89c2c1bc56839
file00 <51076009+file00@users.noreply.github.com>
2026-06-07 23:02:35 +0900

    Update Single-line Comments in C.c

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Single-line Comments in C.c
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Single-line Comments in C.c
@@ -1,13 +1,16 @@
 // Single-line Comments in C 
 
+// Note: In C, any text after '//' on the same line is ignored by the compiler.
+// That is why these are called "single-line" or "inline" comments.
+
 #include <stdio.h>
 
 int main() {
   
-  // create integer variable    
+  // (This is a single-line comment) create integer variable    
   int age = 25; 
  
-  printf("Age: %d", age);   // print the age variable
+  printf("Age: %d", age);   // (This is also a single-line comment) print the age variable
 
   return 0;
 }
