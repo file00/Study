c451889bdb72dfbd60b167547dada3f57a2a1b85
file00 <51076009+file00@users.noreply.github.com>
2026-06-07 22:58:03 +0900

    Update My First C Program (Input & Output Version).c

--- C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (Input & Output Version).c
+++ C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/My First C Program (Input & Output Version).c
@@ -6,6 +6,7 @@ int main() {
     
     char greeting[50];
 
+    // fgets safely reads a line of text from the keyboard (stdin), ensuring it does not exceed the 50-byte limit of the greeting array.
     fgets(greeting, sizeof(greeting), stdin);
     printf("%s", greeting);
 
