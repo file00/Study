2f283f57dbf3cb602d8b2f498c33d5d6ef5cb5a3
file00 <51076009+file00@users.noreply.github.com>
2026-06-10 08:07:41 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java for-each Loop/print Array Elements.java
@@ -0,0 +1,15 @@
+// print Array Elements 
+
+public class Main {
+    
+  public static void main(String[] args) {
+      
+    // create an array
+    int[] numbers = {3, 9, 5, -5};
+    
+    // for each loop 
+    for (int number: numbers) {
+      System.out.println(number);
+    }
+  }
+}
