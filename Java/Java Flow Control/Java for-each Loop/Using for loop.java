6e0b27f8cd6a9060c89223b5fafe04e19d02a50c
file00 <51076009+file00@users.noreply.github.com>
2026-06-17 06:32:07 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java for-each Loop/Using for loop.java
@@ -0,0 +1,15 @@
+// Using for loop
+
+public class Main {
+    
+ public static void main(String[] args) {
+    
+   char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+   // iterating through an array using a for loop
+   for (int i = 0; i < vowels.length; ++ i) {
+     System.out.println(vowels[i]);
+   }
+   
+ }
+}
