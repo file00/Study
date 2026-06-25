d28dd4a798d773884c55f3678cf2c10a9a3932f9
file00 <51076009+file00@users.noreply.github.com>
2026-06-26 06:18:06 +0900

    Add files via upload

--- Java/Java Flow Control/Java while and do while Loop/Display Numbers from 1 to 5.java
+++ Java/Java Flow Control/Java while and do while Loop/Display Numbers from 1 to 5.java
@@ -1,18 +1,19 @@
 // Display Numbers from 1 to 5
-// Program to display numbers from 1 to 5
+// Java Program to display numbers from 1 to 5
+
+import java.util.Scanner;
+
+// Program to find the sum of natural numbers from 1 to 100.
 
 public class Main {
-    
   public static void main(String[] args) {
 
-    // declare variables
     int i = 1, n = 5;
 
-    // while loop from 1 to 5
-    while(i <= n) {
+    // do...while loop from 1 to 5
+    do {
       System.out.println(i);
       i++;
-    }
-    
+    } while(i <= n);
   }
 }
