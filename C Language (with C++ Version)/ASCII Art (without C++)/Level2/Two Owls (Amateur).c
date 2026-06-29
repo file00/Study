ce309f7949c11f2e7891a6dbb96c58bbe891f5a7
file00 <51076009+file00@users.noreply.github.com>
2026-06-30 00:08:24 +0900

    Update Two Owls (Amateur).c

--- C Language (with C++ Version)/ASCII Art (without C++)/Level2/Two Owls (Amateur).c
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level2/Two Owls (Amateur).c
@@ -1,12 +1,15 @@
-// Two Owls (ASCII Art)
+// [Title] Two Owls (ASCII Art)
+
 #include <stdio.h>
 
 int main() {
+    
     printf("   /\\___/\\       /\\___/\\   \n");
     printf("  (  o o  )     (  o o  )  \n");
     printf("  (   v   )     (   v   )  \n");
     printf("  /   ^   \\     /   ^   \\  \n");
     printf(" ( (     ) )   ( (     ) ) \n");
     printf("  \\_______/     \\_______/  \n");
+    
     return 0;
 }
