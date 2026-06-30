39c526020b8844b4df7d3cf60d70a55bcf932f01
file00 <51076009+file00@users.noreply.github.com>
2026-07-01 07:49:53 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/A Book.c
@@ -0,0 +1,14 @@
+// [Title] A Book
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("    _______\n");
+    printf("   /      /,\n");
+    printf("  /      //\n");
+    printf(" /______//\n");
+    printf("(______([\n"); 
+
+    return 0;
+}
