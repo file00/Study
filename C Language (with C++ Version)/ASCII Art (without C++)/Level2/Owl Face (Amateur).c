8cb0484d6d4cd36f99f750eb702ef464e0111788
file00 <51076009+file00@users.noreply.github.com>
2026-07-02 08:18:53 +0900

    Update Owl Face (Amateur).c

--- C Language (with C++ Version)/ASCII Art (without C++)/Level2/Owl Face (Amateur).c
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level2/Owl Face (Amateur).c
@@ -1,12 +1,15 @@
-// Owl Face (ASCII Art)
+// [Title] Owl Face (ASCII Art)
+
 #include <stdio.h>
 
 int main() {
+
     printf("   /\\___/\\   \n");
     printf("  (  o o  )  \n");
     printf("  (   v   )  \n");
     printf("  /   ^   \\  \n");
     printf(" ( (     ) ) \n");
     printf("  \\_______/  \n");
+    
     return 0;
 }
