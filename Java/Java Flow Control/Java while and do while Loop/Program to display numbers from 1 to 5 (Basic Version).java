5180238261c83104e61dc0eb7b88f8aad0f169ac
file00 <51076009+file00@users.noreply.github.com>
2026-07-05 00:14:50 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java while and do while Loop/Program to display numbers from 1 to 5 (Basic Version).java
@@ -0,0 +1,16 @@
+// Program to display numbers from 1 to 5 (Basic Version)
+
+public class Main {
+    
+  public static void main(String[] args) {
+
+    // declare variables
+    int i = 1, n = 5;
+
+    // while loop from 1 to 5
+    while(i <= n) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
