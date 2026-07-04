f412db61be57d69fa4dffe086bceddceece822fc
file00 <51076009+file00@users.noreply.github.com>
2026-07-05 00:16:11 +0900

    Add files via upload

--- Java/Java Flow Control/Java while and do while Loop/Sum of Positive Numbers.java
+++ Java/Java Flow Control/Java while and do while Loop/Sum of Positive Numbers.java
@@ -1,5 +1,6 @@
-// Sum of Positive Numbers
+//  Sum of Positive Numbers
 // Java program to find the sum of positive numbers
+
 import java.util.Scanner;
 
 public class Main {
@@ -22,7 +23,6 @@ public class Main {
     } while(number >= 0); 
 	   
     System.out.println("Sum = " + sum);
-    
     input.close();
   }
 }
