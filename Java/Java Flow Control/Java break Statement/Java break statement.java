107a9930e4b563bdee41f4e060b291e33e3d69bb
file00 <51076009+file00@users.noreply.github.com>
2026-07-13 05:57:41 +0900

    Add files via upload

---
+++ Java/Java Flow Control/Java break Statement/Java break statement.java
@@ -0,0 +1,17 @@
+// Java break statement
+
+public class Main {
+    
+    public static void main(String[] args) {
+      
+        // for loop
+        for (int i = 1; i <= 10; ++i) {
+
+            // if the value of i is 5 the loop terminates  
+            if (i == 5) {
+                break;
+            }      
+            System.out.println(i);
+        }   
+    }
+}
