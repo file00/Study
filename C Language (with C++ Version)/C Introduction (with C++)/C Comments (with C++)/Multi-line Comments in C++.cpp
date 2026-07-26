5370206e0f1aee42cd463c61354096049ac40e97
file00 <51076009+file00@users.noreply.github.com>
2026-07-27 06:14:37 +0900

    Update Multi-line Comments in C++.cpp

--- C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Multi-line Comments in C++.cpp
+++ C Language (with C++ Version)/C Introduction (with C++)/C Comments (with C++)/Multi-line Comments in C++.cpp
@@ -1,7 +1,6 @@
 // [Title] Multi-line Comments in C++ 
 // This program takes age input from the user
 // It stores it in the age variable and, print the value using cout() 
-
 #include <iostream>
 using namespace std;
 
