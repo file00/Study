8a628150cdb3f0b3d30fde94465d412da1a15e76
file00 <51076009+file00@users.noreply.github.com>
2026-08-02 10:02:44 +0900

    Update and rename My First C Program (with Escape Sequence).c to Exam Prep - Analysis of Output with Escape Sequences.c

---
+++ C Language (with C++ Version)/C Introduction (with C++)/My First C Program (with C++)/Exam Prep - Analysis of Output with Escape Sequences.c
@@ -0,0 +1,15 @@
+// [Title] Exam Prep - Analysis of Output with Escape Sequences
+#include <stdio.h>
+
+int main() {
+    // [Step 1] Print first string without a newline sequence
+    printf("I love C Language!!"); 
+    
+    // [Step 2] Print second string with a newline and a horizontal tab sequence
+    printf("Hello,\tWorld!\n");   
+
+    // [Step 3] Print third string on the new line
+    printf("Welcome to Exam");
+
+    return 0;
+}
