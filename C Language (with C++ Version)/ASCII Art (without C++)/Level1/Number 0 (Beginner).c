5f72c81411c518943c8a5657ab0211217b65e5e2
file00 <51076009+file00@users.noreply.github.com>
2026-08-11 08:21:31 +0900

    Update Number 0 (Beginner).c

--- C Language (with C++ Version)/ASCII Art (without C++)/Level1/Number 0 (Beginner).c
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Number 0 (Beginner).c
@@ -1,16 +1,15 @@
 // [Title] Number 0 (ASCII Art)
-
 #include <stdio.h>
 
 int main() {
     
-    printf("    00000    \n");
-    printf("   0     0   \n");
-    printf("   0     0   \n");
-    printf("   0     0   \n");
-    printf("   0     0   \n");
-    printf("   0     0   \n");
-    printf("    00000    \n");
+    printf("    000000    \n");
+    printf("   0      0   \n");
+    printf("   0      0   \n");
+    printf("   0      0   \n");
+    printf("   0      0   \n");
+    printf("   0      0   \n");
+    printf("    000000    \n");
     
     return 0;
 }
