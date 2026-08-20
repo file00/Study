21310a1dd2d6a1ff952b377a59f54d3fb1f9651a
file00 <51076009+file00@users.noreply.github.com>
2026-08-21 07:17:43 +0900

    Add files via upload

---
+++ SQL/SQL Introduction/Read Data From a Table.sql
@@ -0,0 +1,2 @@
+-- Read Data From a Table
+SELECT first_name, last_name FROM Customers;
---
+++ SQL/SQL Introduction/Run my First SQL Query.sql
@@ -0,0 +1,8 @@
+-- Run my First SQL Query
+
+CREATE DATABASE subjects;
+
+CREATE TABLE subjects.Math (
+student_marks INT,
+student_name VARCHAR (255)
+)
