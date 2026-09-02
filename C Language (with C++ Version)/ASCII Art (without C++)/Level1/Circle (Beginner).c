4fdc7da54a100b4b546eabcb52d70f78f0772576
file00 <51076009+file00@users.noreply.github.com>
2026-09-03 08:54:45 +0900

    Update Circle (Beginner).c

--- C Language (with C++ Version)/ASCII Art (without C++)/Level1/Circle (Beginner).c
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Circle (Beginner).c
@@ -3,12 +3,12 @@
 
 int main() {
     
-    printf("    ****    \n");
-    printf("  *      *  \n");
-    printf(" *        * \n");
-    printf(" *        * \n");
-    printf("  *      *  \n");
-    printf("    ****    \n");
+    printf("    *****    \n");
+    printf("  *       *  \n");
+    printf(" *         * \n");
+    printf(" *         * \n");
+    printf("  *       *  \n");
+    printf("    *****    \n");
     
     return 0;
 }
