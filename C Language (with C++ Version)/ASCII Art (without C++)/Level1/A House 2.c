dabe0b0365e666537ae21da75ed9dcc95c68a4f1
file00 <51076009+file00@users.noreply.github.com>
2026-09-04 06:51:53 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/A House 2.c
@@ -0,0 +1,24 @@
+// [Title] A House 2
+#include <stdio.h>
+
+int main() {
+    printf("       .---.          \n");
+    printf("      /     \\     *  \n");
+    printf("      \\_.._/          \n");
+    printf("       _||_           \n");
+    printf("      (_/\\_)    ,     \n");
+    printf("        ||     /|     \n");
+    printf("     .-'\"\"'-. / |     \n");
+    printf("    /________\\\\ |  ~  \n");
+    printf("    |   __   | \\|     \n");
+    printf("    |  |  |  |        \n");
+    printf("    |  |  |  |        \n");
+    printf("    |  |__|  |        \n");
+    printf("   ==============     \n");
+    printf("  /              \\    \n");
+    printf(" /________________\\   \n");
+    printf(" ~~~~~~~~~~~~~~~~~~~  \n");
+    printf("  ~   ~   ~   ~   ~   \n");
+
+    return 0;
+}
