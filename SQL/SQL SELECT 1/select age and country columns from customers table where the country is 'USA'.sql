3f76f9d2a275b0078fb3dccc70f053521c00767e
file00 <51076009+file00@users.noreply.github.com>
2026-09-08 07:56:01 +0900

    Add files via upload

---
+++ SQL/SQL SELECT 1/select age and country columns from customers table where the country is 'USA'.sql
@@ -0,0 +1,4 @@
+-- select age and country columns from customers table where the country is 'USA' 
+SELECT age, country
+FROM Customers
+WHERE country = 'USA';
---
+++ SQL/SQL SELECT 1/select all columns from the customers table with last_name 'Doe'.sql
@@ -0,0 +1,4 @@
+-- select all columns from the customers table with last_name 'Doe' 
+SELECT *
+FROM Customers
+WHERE last_name = 'Doe';
