5ac63f3e9ff34ad95a5133b46f4a1480536882cb
file00 <51076009+file00@users.noreply.github.com>
2026-09-10 08:28:32 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Cross.c
@@ -0,0 +1,18 @@
+// [Title] Cross
+#include <stdio.h>
+
+int main() {
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("    ######[####]######    \n");
+    printf("    ######[####]######    \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+    printf("          [####]          \n");
+
+    return 0;
+}
