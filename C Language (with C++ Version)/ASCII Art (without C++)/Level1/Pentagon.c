1712c89c658fea3ce70237838ff6b6fa0341a403
file00 <51076009+file00@users.noreply.github.com>
2026-09-10 10:48:54 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Pentagon.c
@@ -0,0 +1,19 @@
+// [Title] Pentagon
+#include <stdio.h>
+
+int main() {
+    
+    // Top section increasing in width
+    printf("         *         \n");
+    printf("        ***        \n");
+    printf("       *****       \n");
+    printf("      *******      \n");
+    printf("     *********     \n");
+    
+    // Bottom section decreasing in width
+    printf("     *********     \n");
+    printf("      *******      \n");
+    printf("       *****       \n");
+
+    return 0;
+}
