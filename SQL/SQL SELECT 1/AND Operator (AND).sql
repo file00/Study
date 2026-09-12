9987a2b0c0acab5f46d9253e86786b89d89852b8
file00 <51076009+file00@users.noreply.github.com>
2026-09-13 08:06:05 +0900

    Add files via upload

---
+++ SQL/SQL SELECT 1/AND Operator (AND).sql
@@ -0,0 +1,5 @@
+-- [Title] AND Operator (AND)
+-- select all columns from Customers table with last_name 'Doe' and country 'USA'
+SELECT *
+FROM Customers
+WHERE last_name = 'Doe' AND country = 'USA';
