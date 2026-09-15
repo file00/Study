d27852e0939c1f5aaa2e06ac8cc17568cf3bd78d
file00 <51076009+file00@users.noreply.github.com>
2026-09-16 06:36:01 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Rotated Hexagon 45 Degrees.c
@@ -0,0 +1,22 @@
+// [Title] Rotated Hexagon 45 Degrees
+#include <stdio.h>
+
+int main() {
+    
+    // Top section: Pointy top increasing in width
+    printf("         *         \n");
+    printf("        ***        \n");
+    printf("       *****       \n");
+    printf("      *******      \n");
+    
+    // Middle section: Straight vertical walls
+    printf("      *******      \n");
+    
+    // Bottom section: Decreasing in width to a point
+    printf("      *******      \n");
+    printf("       *****       \n");
+    printf("        ***        \n");
+    printf("         *         \n");
+
+    return 0;
+}
