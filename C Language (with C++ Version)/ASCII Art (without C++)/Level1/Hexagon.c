5a03917191c263190eaf0e607a7349297f1da105
file00 <51076009+file00@users.noreply.github.com>
2026-09-16 06:34:26 +0900

    Update and rename Pentagon.c to Hexagon.c

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Hexagon.c
@@ -0,0 +1,21 @@
+// [Title] Hexagon
+#include <stdio.h>
+
+int main() {
+    
+    // Top section: Increasing in width
+    printf("      *******      \n");
+    printf("     *********     \n");
+    printf("    ***********    \n");
+    
+    // Middle section: Maximum width
+    printf("   *************   \n");
+    printf("   *************   \n");
+    
+    // Bottom section: Decreasing in width
+    printf("    ***********    \n");
+    printf("     *********     \n");
+    printf("      *******      \n");
+
+    return 0;
+}
