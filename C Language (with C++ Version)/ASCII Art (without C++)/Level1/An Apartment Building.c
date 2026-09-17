5a059f8391df5c7b441774b30f7d054cd209d9fa
file00 <51076009+file00@users.noreply.github.com>
2026-09-17 21:43:14 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/An Apartment Building.c
@@ -0,0 +1,36 @@
+// [Title] An Apartment Building
+#include <stdio.h>
+
+int main() {
+    
+    // Cloud and Bird layout above the building
+    printf("         _  _                           \n");
+    printf("       ( `   )_         _               \n");
+    printf("      (    )   `)     _(_)_             \n");
+    printf("    (_   (_ .  _)    (_)_)              \n");
+    printf("                                        \n");
+    // Main building structure
+    printf("     _______________________            \n");
+    printf("    |_______________________|           \n");
+    printf("    |  ___   ___   ___   ___  |         \n");
+    printf("    | |_|_| |_|_| |_|_| |_|_| |         \n"); // 4th Floor
+    printf("    |  ___   ___   ___   ___  |         \n");
+    printf("    | |_|_| |_|_| |_|_| |_|_| |         \n"); // 3rd Floor
+    printf("    |  ___   ___   ___   ___  |         \n");
+    printf("    | |_|_| |_|_| |_|_| |_|_| |         \n"); // 2nd Floor
+    printf("    |  ___   ___   ___   ___  |         \n");
+    printf("    | |_|_| |_|_| |_|_| |_|_| |         \n"); // 1st Floor
+    printf("    |      _______            |         \n");
+    printf("    |     |   |   |   [___]   |         \n"); // Lobby & Security office
+    printf("   _|_____|___|___|___________|_        \n");
+    printf("  |=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|       \n");
+    // Surrounding environment
+    printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+    printf("          ||        ______              \n");
+    printf("        __||__     /      \\             \n"); // Street lamp and Tree
+    printf("       [______]   |   ()   |            \n");
+    printf("          ||       \\______/             \n");
+    printf("          ||          ||                \n");
+
+    return 0;
+}
