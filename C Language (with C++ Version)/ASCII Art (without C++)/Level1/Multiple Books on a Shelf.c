dafd82511e21b67747fc5d85ae26c7b0fdaa3b3b
file00 <51076009+file00@users.noreply.github.com>
2026-09-19 07:46:05 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Multiple Books on a Shelf.c
@@ -0,0 +1,18 @@
+// [Title] Multiple Books on a Shelf
+#include <stdio.h>
+
+int main() {
+    
+    // Displaying a row of books standing together
+    printf("     _______________________________ \n");
+    printf("    /      /      /      /      /  /|\n");
+    printf("   /      /      /      /      /  / |\n");
+    printf("  /______/______/______/______/  /  |\n");
+    printf(" |      |      |      |      |  /   |\n");
+    printf(" |  B   |  O   |  O   |  K   | /    |\n");
+    printf(" |  1   |  2   |  3   |  4   |/     /\n");
+    printf(" |______|______|______|______|/_____/\n");
+    printf(" (______((______((______((______([   \n");
+
+    return 0;
+}
