dfed9790a44267c325e0e13922335effd6132863
file00 <51076009+file00@users.noreply.github.com>
2026-09-21 06:40:30 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Octagon.c
@@ -0,0 +1,22 @@
+// [Title] Octagon
+#include <stdio.h>
+
+int main() {
+    
+    // Top section: Slanted edges increasing in width
+    printf("      *********      \n");
+    printf("     ***********     \n");
+    printf("    *************    \n");
+    
+    // Middle section: Straight vertical edges
+    printf("   ***************   \n");
+    printf("   ***************   \n");
+    printf("   ***************   \n");
+    
+    // Bottom section: Slanted edges decreasing in width
+    printf("    *************    \n");
+    printf("     ***********     \n");
+    printf("      *********      \n");
+
+    return 0;
+}
