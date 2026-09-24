61ad38d64fd188631761d2d35ad86c66889d5a0d
file00 <51076009+file00@users.noreply.github.com>
2026-09-25 07:38:41 +0900

    Add files via upload

---
+++ C Language (with C++ Version)/ASCII Art (without C++)/Level1/Alphabet A (ASCII Art).c
@@ -0,0 +1,17 @@
+// [Title] Alphabet A (ASCII Art)
+#include <stdio.h>
+
+int main() {
+    
+    printf("      AA      \n");
+    printf("     A  A     \n");
+    printf("    A    A    \n");
+    printf("   A      A   \n");
+    printf("   AAAAAAAA   \n");
+    printf("   A      A   \n");
+    printf("   A      A   \n");
+    printf("   A      A   \n");
+    printf("   A      A   \n");
+    
+    return 0;
+}
